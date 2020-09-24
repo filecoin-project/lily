dc82c6cfa3ee695ac34d587042c8b231174f7701
lanzafame <adrianlanzafame92@gmail.com>
2020-09-24 12:13:14 +1000

    add breakdown of messages persist

--- model/messages/blockmessage.go
+++ model/messages/blockmessage.go
@@ -3,11 +3,17 @@ package messages
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/go-pg/pg/v10"
+	"go.opencensus.io/stats"
+	"go.opencensus.io/tag"
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/trace"
 	"go.opentelemetry.io/otel/label"
+
+	"github.com/filecoin-project/sentinel-visor/metrics"
+	"github.com/filecoin-project/sentinel-visor/tasks"
 )
 
 type BlockMessage struct {
@@ -29,6 +35,13 @@ type BlockMessages []*BlockMessage
 func (bms BlockMessages) PersistWithTx(ctx context.Context, tx *pg.Tx) error {
 	ctx, span := global.Tracer("").Start(ctx, "BlockMessages.PersistWithTx", trace.WithAttributes(label.Int("count", len(bms))))
 	defer span.End()
+
+	ctx, _ = tag.New(ctx, tag.Upsert(metrics.TaskNS, fmt.Sprintf("%s_%s", tasks.MessagePoolName, "block_messages")))
+	start := time.Now()
+	defer func() {
+		stats.Record(ctx, metrics.PersistDuration.M(metrics.SinceInMilliseconds(start)))
+	}()
+
 	for _, bm := range bms {
 		if err := bm.PersistWithTx(ctx, tx); err != nil {
 			return err
