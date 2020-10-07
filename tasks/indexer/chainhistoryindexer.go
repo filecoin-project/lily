filecoin-project/lily

commit 3b987082ab290c2a0ce07e71cd616c1ae73e4a17

feat: compute gas outputs (#67)
This change adds a gas outputs processing task. The results of the task are written to
a new table derived_gas_outputs. The task fetches batches of messages that have receipts
and calls the lotus vm ComputeGasOutputs method, persisting the results.

This change also performs a significant refactor of the visor processing tables. Previously
the chain indexers would write duplicate rows to visor_processing_statechanges and
visor_processing_messages. Each of those tables contained claimed_until, completed_at and
errors_detected columns. They were used by the ActorStateChange and Message tasks. However
if we were to add another task that processed tipsets we would need to add another processing
table and then reindex the entire chain to populate it.

To avoid this, the visor_processing_statechanges and visor_processing_messages tables have
been combined into a single visor_processing_tipsets table. Each task now has a set of three
columns in this table to track its work: statechange_claimed_until/statechange_completed_at/
statechange_errors_detected and message_claimed_until/message_completed_at/message_errors_detected.

Adding a new tipset-oriented task is then a matter of adding three columns to this table.

The visor_processing_messages table has been repurposed to track work that needs to be performed
on each message seen. The GasOutputs task is the first task to use this table. It uses the
gas_outputs_claimed_until, gas_outputs_completed_at and gas_outputs_errors_detected columns.

Additional message-oriented tasks can be supported by adding new columns to this table, avoiding
the need to read every message in the chain again.

tasks/indexer/chainhistoryindexer.go
@@ -80,6 +80,7 @@ func (c *ChainHistoryIndexer) WalkChain(ctx context.Context, maxHeight int64) er
 		return xerrors.Errorf("get chain head: %w", err)
 	}
 
+	log.Debugw("head", "height", head.Height())
 	toVisit.PushBack(head)
 
 	// TODO: revisit this loop which was designed to collect blocks but could now be a lot simpler since we are
@@ -92,6 +93,18 @@ func (c *ChainHistoryIndexer) WalkChain(ctx context.Context, maxHeight int64) er
 		}
 
 		ts := toVisit.Remove(toVisit.Back()).(*types.TipSet)
+		if ts.Height() == 0 {
+			continue
+		}
+
+		// TODO: Look for websocket connection closed error and retry after a delay to avoid hot loop
+		pts, err := c.node.ChainGetTipSet(ctx, ts.Parents())
+		if err != nil {
+			return xerrors.Errorf("get tipset: %w", err)
+		}
+
+		toVisit.PushBack(pts)
+
 		if blockData.Seen(ts.Key()) {
 			continue
 		}
@@ -107,19 +120,9 @@ func (c *ChainHistoryIndexer) WalkChain(ctx context.Context, maxHeight int64) er
 			blockData.Reset()
 		}
 
-		if ts.Height() == 0 {
-			continue
-		}
-		// TODO: Look for websocket connection closed error and retry after a delay to avoid hot loop
-		pts, err := c.node.ChainGetTipSet(ctx, ts.Parents())
-		if err != nil {
-			return xerrors.Errorf("get tipset: %w", err)
-		}
-
-		toVisit.PushBack(pts)
 	}
 
-	log.Debugw("persisting final batch", "count", blockData.Size(), "toVisit", toVisit.Len())
+	log.Debugw("persisting final batch", "count", blockData.Size(), "height", blockData.Height())
 	if err := blockData.Persist(ctx, c.storage.DB); err != nil {
 		return xerrors.Errorf("persist: %w", err)
 	}

tasks/indexer/chainhistoryindexer_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/filecoin-project/sentinel-visor/lens/lotus"
 	"github.com/filecoin-project/sentinel-visor/model/blocks"
 	"github.com/filecoin-project/sentinel-visor/model/visor"
 	"github.com/filecoin-project/sentinel-visor/storage"
@@ -36,7 +37,9 @@ func TestChainHistoryIndexer(t *testing.T) {
 
 	t.Logf("preparing chain")
 	nodes, sn := nodetest.Builder(t, 1, apitest.OneMiner)
-	node := nodeWrapper{TestNode: nodes[0]}
+	cs, err := lotus.NewCacheCtxStore(ctx, nodes[0], 5)
+	require.NoError(t, err, "cache store")
+	node := lotus.NewAPIWrapper(nodes[0], cs)
 
 	apitest.MineUntilBlock(ctx, t, nodes[0], sn[0], nil)
 
@@ -117,27 +120,13 @@ func TestChainHistoryIndexer(t *testing.T) {
 		}
 	})
 
-	t.Run("visor_processing_statechanges", func(t *testing.T) {
+	t.Run("visor_processing_tipsets", func(t *testing.T) {
 		var count int
-		_, err := db.QueryOne(pg.Scan(&count), `SELECT COUNT(*) FROM visor_processing_statechanges`)
+		_, err := db.QueryOne(pg.Scan(&count), `SELECT COUNT(*) FROM visor_processing_tipsets`)
 		require.NoError(t, err)
 		assert.Equal(t, len(tipSetKeys), count)
 
-		var m *visor.ProcessingStateChange
-		for _, tsk := range tipSetKeys {
-			exists, err := db.Model(m).Where("tip_set = ?", tsk).Exists()
-			require.NoError(t, err)
-			assert.True(t, exists, "tsk: %s", tsk)
-		}
-	})
-
-	t.Run("visor_processing_messages", func(t *testing.T) {
-		var count int
-		_, err := db.QueryOne(pg.Scan(&count), `SELECT COUNT(*) FROM visor_processing_messages`)
-		require.NoError(t, err)
-		assert.Equal(t, len(tipSetKeys), count)
-
-		var m *visor.ProcessingMessage
+		var m *visor.ProcessingTipSet
 		for _, tsk := range tipSetKeys {
 			exists, err := db.Model(m).Where("tip_set = ?", tsk).Exists()
 			require.NoError(t, err)

commands/run.go
@@ -70,7 +70,7 @@ var Run = &cli.Command{
 		&cli.IntFlag{
 			Name:    "statechange-workers",
 			Aliases: []string{"scw"},
-			Value:   5,
+			Value:   15,
 			Usage:   "Number of actor state change processors to start",
 			EnvVars: []string{"VISOR_STATECHANGE_WORKERS"},
 		},
@@ -92,7 +92,7 @@ var Run = &cli.Command{
 		&cli.IntFlag{
 			Name:    "actorstate-workers",
 			Aliases: []string{"asw"},
-			Value:   5,
+			Value:   15,
 			Usage:   "Number of actor state processors to start",
 			EnvVars: []string{"VISOR_ACTORSTATE_WORKERS"},
 		},
@@ -126,9 +126,31 @@ var Run = &cli.Command{
 		&cli.IntFlag{
 			Name:    "message-workers",
 			Aliases: []string{"mw"},
-			Value:   5,
-			Usage:   "Number of message to start",
+			Value:   15,
+			Usage:   "Number of message processors to start",
 			EnvVars: []string{"VISOR_MESSAGE_WORKERS"},
+		},
+
+		&cli.DurationFlag{
+			Name:    "gasoutputs-lease",
+			Aliases: []string{"gol"},
+			Value:   time.Minute * 15,
+			Usage:   "Lease time for the gas outputs processor",
+			EnvVars: []string{"VISOR_GASOUTPUTS_LEASE"},
+		},
+		&cli.IntFlag{
+			Name:    "gasoutputs-batch",
+			Aliases: []string{"gob"},
+			Value:   500, // can be high because we don't hit the lotus api
+			Usage:   "Batch size for the gas outputs processor",
+			EnvVars: []string{"VISOR_GASOUTPUTS_BATCH"},
+		},
+		&cli.IntFlag{
+			Name:    "gasoutputs-workers",
+			Aliases: []string{"gow"},
+			Value:   15,
+			Usage:   "Number of gas outputs processors to start",
+			EnvVars: []string{"VISOR_GASOUTPUTS_WORKERS"},
 		},
 	},
 	Action: func(cctx *cli.Context) error {
@@ -181,7 +203,7 @@ var Run = &cli.Command{
 				Task:                indexer.NewChainHistoryIndexer(rctx.db, rctx.api),
 				Locker:              NewGlobalSingleton(ChainHistoryIndexerLockID, rctx.db), // only want one history indexer anywhere to be running
 				RestartOnFailure:    true,
-				RestartOnCompletion: false, // run once only
+				RestartOnCompletion: true,
 			})
 		}
 
@@ -214,6 +236,16 @@ var Run = &cli.Command{
 			scheduler.Add(schedule.TaskConfig{
 				Name:                fmt.Sprintf("MessageProcessor%03d", i),
 				Task:                message.NewMessageProcessor(rctx.db, rctx.api, cctx.Duration("message-lease"), cctx.Int("message-batch"), heightFrom, heightTo),
+				RestartOnFailure:    true,
+				RestartOnCompletion: true,
+			})
+		}
+
+		// Add several gas output tasks to read gas outputs from indexed messages
+		for i := 0; i < cctx.Int("gasoutputs-workers"); i++ {
+			scheduler.Add(schedule.TaskConfig{
+				Name:                fmt.Sprintf("GasOutputsProcessor%03d", i),
+				Task:                message.NewGasOutputsProcessor(rctx.db, rctx.api, cctx.Duration("gasoutputs-lease"), cctx.Int("gasoutputs-batch"), heightFrom, heightTo),
 				RestartOnFailure:    true,
 				RestartOnCompletion: true,
 			})

commands/setup.go
@@ -156,8 +156,8 @@ func setupLogging(cctx *cli.Context) error {
 }
 
 const (
-	ChainHeadIndexerLockID    = 9898981111
-	ChainHistoryIndexerLockID = 9898981112
+	ChainHeadIndexerLockID    = 98981111
+	ChainHistoryIndexerLockID = 98981112
 )
 
 func NewGlobalSingleton(id int64, d *storage.Database) *GlobalSingleton {

lens/interface.go
@@ -1,11 +1,14 @@
 package lens
 
 import (
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/vm"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
 type API interface {
 	Store() adt.Store
 	api.FullNode
+	ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.TokenAmount) vm.GasOutputs
 }

lens/lotus/api.go
@@ -8,9 +8,11 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-bitfield"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
 	miner "github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/vm"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 
 	"github.com/filecoin-project/sentinel-visor/lens"
@@ -122,4 +124,8 @@ func (aw *APIWrapper) StateReadState(ctx context.Context, actor address.Address,
 	ctx, span := global.Tracer("").Start(ctx, "Lotus.StateReadState")
 	defer span.End()
 	return aw.FullNode.StateReadState(ctx, actor, tsk)
+}
+
+func (aw *APIWrapper) ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.TokenAmount) vm.GasOutputs {
+	return vm.ComputeGasOutputs(gasUsed, gasLimit, baseFee, feeCap, gasPremium)
 }

model/derived/gasoutputs.go
@@ -1,0 +1,64 @@
+package derived
+
+import (
+	"context"
+
+	"github.com/go-pg/pg/v10"
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/api/trace"
+	"go.opentelemetry.io/otel/label"
+	"golang.org/x/xerrors"
+)
+
+type GasOutputs struct {
+	tableName          struct{} `pg:"derived_gas_outputs"`
+	Cid                string   `pg:",pk,notnull"`
+	From               string   `pg:",notnull"`
+	To                 string   `pg:",notnull"`
+	Value              string   `pg:",notnull"`
+	GasFeeCap          string   `pg:",notnull"`
+	GasPremium         string   `pg:",notnull"`
+	GasLimit           int64    `pg:",use_zero,notnull"`
+	SizeBytes          int      `pg:",use_zero,notnull"`
+	Nonce              uint64   `pg:",use_zero,notnull"`
+	Method             uint64   `pg:",use_zero,notnull"`
+	StateRoot          string   `pg:",notnull"`
+	ExitCode           int64    `pg:",use_zero,notnull"`
+	GasUsed            int64    `pg:",use_zero,notnull"`
+	ParentBaseFee      string   `pg:",notnull"`
+	BaseFeeBurn        string   `pg:",notnull"`
+	OverEstimationBurn string   `pg:",notnull"`
+	MinerPenalty       string   `pg:",notnull"`
+	MinerTip           string   `pg:",notnull"`
+	Refund             string   `pg:",notnull"`
+	GasRefund          int64    `pg:",use_zero,notnull"`
+	GasBurned          int64    `pg:",use_zero,notnull"`
+}
+
+func (g *GasOutputs) PersistWithTx(ctx context.Context, tx *pg.Tx) error {
+	if _, err := tx.ModelContext(ctx, g).
+		OnConflict("do nothing").
+		Insert(); err != nil {
+		return xerrors.Errorf("persisting derived gas outputs: %w", err)
+	}
+	return nil
+}
+
+type GasOutputsList []*GasOutputs
+
+func (l GasOutputsList) Persist(ctx context.Context, db *pg.DB) error {
+	return db.RunInTransaction(ctx, func(tx *pg.Tx) error {
+		return l.PersistWithTx(ctx, tx)
+	})
+}
+
+func (l GasOutputsList) PersistWithTx(ctx context.Context, tx *pg.Tx) error {
+	ctx, span := global.Tracer("").Start(ctx, "GasOutputsList.PersistWithTx", trace.WithAttributes(label.Int("count", len(l))))
+	defer span.End()
+	for _, p := range l {
+		if err := p.PersistWithTx(ctx, tx); err != nil {
+			return nil
+		}
+	}
+	return nil
+}

model/messages/task.go
@@ -14,18 +14,25 @@ type MessageTaskResult struct {
 }
 
 func (mtr *MessageTaskResult) Persist(ctx context.Context, db *pg.DB) error {
-	ctx, span := global.Tracer("").Start(ctx, "MessageTaskResult.Persist")
-	defer span.End()
 	return db.RunInTransaction(ctx, func(tx *pg.Tx) error {
-		if err := mtr.Messages.PersistWithTx(ctx, tx); err != nil {
-			return err
-		}
-		if err := mtr.BlockMessages.PersistWithTx(ctx, tx); err != nil {
-			return err
-		}
-		if err := mtr.Receipts.PersistWithTx(ctx, tx); err != nil {
-			return err
-		}
-		return nil
+		return mtr.PersistWithTx(ctx, tx)
 	})
+
+}
+
+func (mtr *MessageTaskResult) PersistWithTx(ctx context.Context, tx *pg.Tx) error {
+	ctx, span := global.Tracer("").Start(ctx, "MessageTaskResult.PersistWithTx")
+	defer span.End()
+
+	if err := mtr.Messages.PersistWithTx(ctx, tx); err != nil {
+		return err
+	}
+	if err := mtr.BlockMessages.PersistWithTx(ctx, tx); err != nil {
+		return err
+	}
+	if err := mtr.Receipts.PersistWithTx(ctx, tx); err != nil {
+		return err
+	}
+
+	return nil
 }

commit 08eb701b50cea1dc556706732ccbdb631018ad66

fix: chain history indexer includes genesis
Commit 3b98708 broke the chain indexer test after a reorganization of the chain
walking logic. It stopped processing when reaching genesis but omitted to
persist the genesis block infomation. This change fixes the bug.

tasks/indexer/chainhistoryindexer.go
@@ -93,17 +93,16 @@ func (c *ChainHistoryIndexer) WalkChain(ctx context.Context, maxHeight int64) er
 		}
 
 		ts := toVisit.Remove(toVisit.Back()).(*types.TipSet)
-		if ts.Height() == 0 {
-			continue
-		}
 
-		// TODO: Look for websocket connection closed error and retry after a delay to avoid hot loop
-		pts, err := c.node.ChainGetTipSet(ctx, ts.Parents())
-		if err != nil {
-			return xerrors.Errorf("get tipset: %w", err)
-		}
+		if ts.Height() != 0 {
+			// TODO: Look for websocket connection closed error and retry after a delay to avoid hot loop
+			pts, err := c.node.ChainGetTipSet(ctx, ts.Parents())
+			if err != nil {
+				return xerrors.Errorf("get tipset: %w", err)
+			}
 
-		toVisit.PushBack(pts)
+			toVisit.PushBack(pts)
+		}
 
 		if blockData.Seen(ts.Key()) {
 			continue

tasks/indexer/chainhistoryindexer_test.go
@@ -7,6 +7,7 @@ import (
 
 	apitest "github.com/filecoin-project/lotus/api/test"
 	nodetest "github.com/filecoin-project/lotus/node/test"
+	logging "github.com/ipfs/go-log/v2"
 
 	"github.com/go-pg/pg/v10"
 	"github.com/stretchr/testify/assert"
@@ -20,6 +21,8 @@ import (
 )
 
 func TestChainHistoryIndexer(t *testing.T) {
+	logging.SetLogLevel("*", "debug")
+
 	if testing.Short() || !testutil.DatabaseAvailable() {
 		t.Skip("short testing requested or VISOR_TEST_DB not set")
 	}