81bbafa97536fc1713bbd9e6c9ca6aa894d5532a
frrist <forrest@protocol.ai>
2020-10-19 13:28:16 -0700

    fix: migration table name

--- storage/migrations/13_parsed_message.go
+++ storage/migrations/13_parsed_message.go
@@ -6,7 +6,7 @@ import (
 
 func init() {
 	up := batch(`
-CREATE TABLE IF NOT EXISTS "message_parsed" (
+CREATE TABLE IF NOT EXISTS "parsed_messages" (
 	"cid" text NOT NULL,
 	"height" bigint NOT NULL,
 	"from" text NOT NULL,
@@ -16,12 +16,12 @@ CREATE TABLE IF NOT EXISTS "message_parsed" (
 	"params" jsonb,
 	PRIMARY KEY ("cid")
 );
-CREATE INDEX IF NOT EXISTS "message_parsed_method_idx" ON public.message_parsed USING HASH ("method");
-CREATE INDEX IF NOT EXISTS "message_parsed_from_idx" ON public.message_parsed USING HASH ("from");
-CREATE INDEX IF NOT EXISTS "message_parsed_to_idx" ON public.message_parsed USING HASH ("to");
+CREATE INDEX IF NOT EXISTS "message_parsed_method_idx" ON public.parsed_messages USING HASH ("method");
+CREATE INDEX IF NOT EXISTS "message_parsed_from_idx" ON public.parsed_messages USING HASH ("from");
+CREATE INDEX IF NOT EXISTS "message_parsed_to_idx" ON public.parsed_messages USING HASH ("to");
 `)
 	down := batch(`
-DROP TABLE IF EXISTS public.message_parsed;
+DROP TABLE IF EXISTS public.parsed_messages;
 DROP INDEX IF EXISTS public.message_parsed_method_idx;
 DROP INDEX IF EXISTS public.message_parsed_from_idx;
 DROP INDEX IF EXISTS public.message_parsed_to_idx;
