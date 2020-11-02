filcloud/sentinel-visor

feat: add power actor claim extraction
closes #171

model/actors/power/task.go
@@ -1,0 +1,41 @@
+package power
+
+import (
+	"context"
+
+	"github.com/go-pg/pg/v10"
+	"go.opentelemetry.io/otel/api/global"
+
+	"github.com/filecoin-project/sentinel-visor/metrics"
+)
+
+type PowerTaskResult struct {
+	PowerStateModel *ChainPower
+	ClaimStateModel PowerActorClaimList
+}
+
+func (p *PowerTaskResult) PersistWithTx(ctx context.Context, tx *pg.Tx) error {
+	if p.PowerStateModel != nil {
+		if err := p.PowerStateModel.PersistWithTx(ctx, tx); err != nil {
+			return err
+		}
+	}
+	if p.ClaimStateModel != nil {
+		if err := p.ClaimStateModel.PersistWithTx(ctx, tx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *PowerTaskResult) Persist(ctx context.Context, db *pg.DB) error {
+	ctx, span := global.Tracer("").Start(ctx, "PowerTaskResult.Persist")
+	defer span.End()
+
+	stop := metrics.Timer(ctx, metrics.PersistDuration)
+	defer stop()
+
+	return db.RunInTransaction(ctx, func(tx *pg.Tx) error {
+		return p.PersistWithTx(ctx, tx)
+	})
+}

tasks/actorstate/power.go
@@ -3,6 +3,8 @@ package actorstate
 import (
 	"context"
 
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/chain/types"
 	"go.opentelemetry.io/otel/api/global"
 	"golang.org/x/xerrors"
 
@@ -25,6 +27,51 @@ func init() {
 	Register(sa2builtin.StoragePowerActorCodeID, StoragePowerExtractor{})
 }
 
+func NewPowerStateExtractionContext(ctx context.Context, a ActorInfo, node ActorStateAPI) (*PowerStateExtractionContext, error) {
+	curActor, err := node.StateGetActor(ctx, a.Address, a.TipSet)
+	if err != nil {
+		return nil, xerrors.Errorf("loading current power actor: %w", err)
+	}
+
+	curTipset, err := node.ChainGetTipSet(ctx, a.TipSet)
+	if err != nil {
+		return nil, xerrors.Errorf("loading current tipset: %w", err)
+	}
+
+	curState, err := power.Load(node.Store(), curActor)
+	if err != nil {
+		return nil, xerrors.Errorf("loading current power state: %w", err)
+	}
+
+	prevState := curState
+	if a.Epoch != 0 {
+		prevActor, err := node.StateGetActor(ctx, a.Address, a.ParentTipSet)
+		if err != nil {
+			return nil, xerrors.Errorf("loading previous power actor at tipset %s epoch %d: %w", a.ParentTipSet, a.Epoch)
+		}
+
+		prevState, err = power.Load(node.Store(), prevActor)
+		if err != nil {
+			return nil, xerrors.Errorf("loading previous power actor state: %w", err)
+		}
+	}
+	return &PowerStateExtractionContext{
+		PrevState: prevState,
+		CurrState: curState,
+		CurrTs:    curTipset,
+	}, nil
+}
+
+type PowerStateExtractionContext struct {
+	PrevState power.State
+	CurrState power.State
+	CurrTs    *types.TipSet
+}
+
+func (p *PowerStateExtractionContext) IsGenesis() bool {
+	return 0 == p.CurrTs.Height()
+}
+
 func (StoragePowerExtractor) Extract(ctx context.Context, a ActorInfo, node ActorStateAPI) (model.Persistable, error) {
 	ctx, span := global.Tracer("").Start(ctx, "StoragePowerExtractor")
 	defer span.End()
@@ -32,39 +79,50 @@ func (StoragePowerExtractor) Extract(ctx context.Context, a ActorInfo, node Acto
 	stop := metrics.Timer(ctx, metrics.ProcessingDuration)
 	defer stop()
 
-	powerActor, err := node.StateGetActor(ctx, power.Address, a.TipSet)
+	ec, err := NewPowerStateExtractionContext(ctx, a, node)
 	if err != nil {
-		return nil, xerrors.Errorf("loading power actor: %w", err)
+		return nil, err
 	}
 
-	pstate, err := power.Load(node.Store(), powerActor)
+	chainPowerModel, err := ExtractChainPower(ec)
 	if err != nil {
-		return nil, xerrors.Errorf("loading power actor state: %w", err)
+		return nil, err
 	}
 
-	locked, err := pstate.TotalLocked()
+	claimedPowerModel, err := ExtractClaimedPower(ec)
 	if err != nil {
 		return nil, err
 	}
-	pow, err := pstate.TotalPower()
+	return &powermodel.PowerTaskResult{
+		PowerStateModel: chainPowerModel,
+		ClaimStateModel: claimedPowerModel,
+	}, nil
+}
+
+func ExtractChainPower(ec *PowerStateExtractionContext) (*powermodel.ChainPower, error) {
+	locked, err := ec.CurrState.TotalLocked()
 	if err != nil {
 		return nil, err
 	}
-	commit, err := pstate.TotalCommitted()
+	pow, err := ec.CurrState.TotalPower()
 	if err != nil {
 		return nil, err
 	}
-	smoothed, err := pstate.TotalPowerSmoothed()
+	commit, err := ec.CurrState.TotalCommitted()
 	if err != nil {
 		return nil, err
 	}
-	participating, total, err := pstate.MinerCounts()
+	smoothed, err := ec.CurrState.TotalPowerSmoothed()
+	if err != nil {
+		return nil, err
+	}
+	participating, total, err := ec.CurrState.MinerCounts()
 	if err != nil {
 		return nil, err
 	}
 
 	return &powermodel.ChainPower{
-		StateRoot:                  a.ParentStateRoot.String(),
+		StateRoot:                  ec.CurrTs.ParentState().String(),
 		TotalRawBytesPower:         pow.RawBytePower.String(),
 		TotalQABytesPower:          pow.QualityAdjPower.String(),
 		TotalRawBytesCommitted:     commit.RawBytePower.String(),
@@ -75,4 +133,48 @@ func (StoragePowerExtractor) Extract(ctx context.Context, a ActorInfo, node Acto
 		MinerCount:                 total,
 		ParticipatingMinerCount:    participating,
 	}, nil
+}
+
+func ExtractClaimedPower(ec *PowerStateExtractionContext) (powermodel.PowerActorClaimList, error) {
+	claimModel := powermodel.PowerActorClaimList{}
+	if ec.IsGenesis() {
+		if err := ec.CurrState.ForEachClaim(func(miner address.Address, claim power.Claim) error {
+			claimModel = append(claimModel, &powermodel.PowerActorClaim{
+				Height:          int64(ec.CurrTs.Height()),
+				StateRoot:       ec.CurrTs.ParentState().String(),
+				MinerID:         miner.String(),
+				RawBytePower:    claim.RawBytePower.String(),
+				QualityAdjPower: claim.QualityAdjPower.String(),
+			})
+			return nil
+		}); err != nil {
+			return nil, err
+		}
+		return claimModel, nil
+	}
+	// normal case.
+	claimChanges, err := power.DiffClaims(ec.PrevState, ec.CurrState)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, newClaim := range claimChanges.Added {
+		claimModel = append(claimModel, &powermodel.PowerActorClaim{
+			Height:          int64(ec.CurrTs.Height()),
+			StateRoot:       ec.CurrTs.ParentState().String(),
+			MinerID:         newClaim.Miner.String(),
+			RawBytePower:    newClaim.Claim.RawBytePower.String(),
+			QualityAdjPower: newClaim.Claim.QualityAdjPower.String(),
+		})
+	}
+	for _, modClaim := range claimChanges.Modified {
+		claimModel = append(claimModel, &powermodel.PowerActorClaim{
+			Height:          int64(ec.CurrTs.Height()),
+			StateRoot:       ec.CurrTs.ParentState().String(),
+			MinerID:         modClaim.Miner.String(),
+			RawBytePower:    modClaim.To.RawBytePower.String(),
+			QualityAdjPower: modClaim.To.QualityAdjPower.String(),
+		})
+	}
+	return claimModel, nil
 }

filecoin-project/lily

model/actors/power/task.go
@@ -1,0 +1,41 @@
+package power
+
+import (
+	"context"
+
+	"github.com/go-pg/pg/v10"
+	"go.opentelemetry.io/otel/api/global"
+
+	"github.com/filecoin-project/sentinel-visor/metrics"
+)
+
+type PowerTaskResult struct {
+	ChainPowerModel *ChainPower
+	ClaimStateModel PowerActorClaimList
+}
+
+func (p *PowerTaskResult) PersistWithTx(ctx context.Context, tx *pg.Tx) error {
+	if p.ChainPowerModel != nil {
+		if err := p.ChainPowerModel.PersistWithTx(ctx, tx); err != nil {
+			return err
+		}
+	}
+	if p.ClaimStateModel != nil {
+		if err := p.ClaimStateModel.PersistWithTx(ctx, tx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *PowerTaskResult) Persist(ctx context.Context, db *pg.DB) error {
+	ctx, span := global.Tracer("").Start(ctx, "PowerTaskResult.Persist")
+	defer span.End()
+
+	stop := metrics.Timer(ctx, metrics.PersistDuration)
+	defer stop()
+
+	return db.RunInTransaction(ctx, func(tx *pg.Tx) error {
+		return p.PersistWithTx(ctx, tx)
+	})
+}

tasks/actorstate/power.go
@@ -3,6 +3,8 @@ package actorstate
 import (
 	"context"
 
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/chain/types"
 	"go.opentelemetry.io/otel/api/global"
 	"golang.org/x/xerrors"
 
@@ -25,6 +27,51 @@ func init() {
 	Register(sa2builtin.StoragePowerActorCodeID, StoragePowerExtractor{})
 }
 
+func NewPowerStateExtractionContext(ctx context.Context, a ActorInfo, node ActorStateAPI) (*PowerStateExtractionContext, error) {
+	curActor, err := node.StateGetActor(ctx, a.Address, a.TipSet)
+	if err != nil {
+		return nil, xerrors.Errorf("loading current power actor: %w", err)
+	}
+
+	curTipset, err := node.ChainGetTipSet(ctx, a.TipSet)
+	if err != nil {
+		return nil, xerrors.Errorf("loading current tipset: %w", err)
+	}
+
+	curState, err := power.Load(node.Store(), curActor)
+	if err != nil {
+		return nil, xerrors.Errorf("loading current power state: %w", err)
+	}
+
+	prevState := curState
+	if a.Epoch != 0 {
+		prevActor, err := node.StateGetActor(ctx, a.Address, a.ParentTipSet)
+		if err != nil {
+			return nil, xerrors.Errorf("loading previous power actor at tipset %s epoch %d: %w", a.ParentTipSet, a.Epoch)
+		}
+
+		prevState, err = power.Load(node.Store(), prevActor)
+		if err != nil {
+			return nil, xerrors.Errorf("loading previous power actor state: %w", err)
+		}
+	}
+	return &PowerStateExtractionContext{
+		PrevState: prevState,
+		CurrState: curState,
+		CurrTs:    curTipset,
+	}, nil
+}
+
+type PowerStateExtractionContext struct {
+	PrevState power.State
+	CurrState power.State
+	CurrTs    *types.TipSet
+}
+
+func (p *PowerStateExtractionContext) IsGenesis() bool {
+	return 0 == p.CurrTs.Height()
+}
+
 func (StoragePowerExtractor) Extract(ctx context.Context, a ActorInfo, node ActorStateAPI) (model.Persistable, error) {
 	ctx, span := global.Tracer("").Start(ctx, "StoragePowerExtractor")
 	defer span.End()
@@ -32,39 +79,50 @@ func (StoragePowerExtractor) Extract(ctx context.Context, a ActorInfo, node Acto
 	stop := metrics.Timer(ctx, metrics.ProcessingDuration)
 	defer stop()
 
-	powerActor, err := node.StateGetActor(ctx, power.Address, a.TipSet)
+	ec, err := NewPowerStateExtractionContext(ctx, a, node)
 	if err != nil {
-		return nil, xerrors.Errorf("loading power actor: %w", err)
+		return nil, err
 	}
 
-	pstate, err := power.Load(node.Store(), powerActor)
+	chainPowerModel, err := ExtractChainPower(ec)
 	if err != nil {
-		return nil, xerrors.Errorf("loading power actor state: %w", err)
+		return nil, err
 	}
 
-	locked, err := pstate.TotalLocked()
+	claimedPowerModel, err := ExtractClaimedPower(ec)
 	if err != nil {
 		return nil, err
 	}
-	pow, err := pstate.TotalPower()
+	return &powermodel.PowerTaskResult{
+		ChainPowerModel: chainPowerModel,
+		ClaimStateModel: claimedPowerModel,
+	}, nil
+}
+
+func ExtractChainPower(ec *PowerStateExtractionContext) (*powermodel.ChainPower, error) {
+	locked, err := ec.CurrState.TotalLocked()
 	if err != nil {
 		return nil, err
 	}
-	commit, err := pstate.TotalCommitted()
+	pow, err := ec.CurrState.TotalPower()
 	if err != nil {
 		return nil, err
 	}
-	smoothed, err := pstate.TotalPowerSmoothed()
+	commit, err := ec.CurrState.TotalCommitted()
 	if err != nil {
 		return nil, err
 	}
-	participating, total, err := pstate.MinerCounts()
+	smoothed, err := ec.CurrState.TotalPowerSmoothed()
+	if err != nil {
+		return nil, err
+	}
+	participating, total, err := ec.CurrState.MinerCounts()
 	if err != nil {
 		return nil, err
 	}
 
 	return &powermodel.ChainPower{
-		StateRoot:                  a.ParentStateRoot.String(),
+		StateRoot:                  ec.CurrTs.ParentState().String(),
 		TotalRawBytesPower:         pow.RawBytePower.String(),
 		TotalQABytesPower:          pow.QualityAdjPower.String(),
 		TotalRawBytesCommitted:     commit.RawBytePower.String(),
@@ -75,4 +133,48 @@ func (StoragePowerExtractor) Extract(ctx context.Context, a ActorInfo, node Acto
 		MinerCount:                 total,
 		ParticipatingMinerCount:    participating,
 	}, nil
+}
+
+func ExtractClaimedPower(ec *PowerStateExtractionContext) (powermodel.PowerActorClaimList, error) {
+	claimModel := powermodel.PowerActorClaimList{}
+	if ec.IsGenesis() {
+		if err := ec.CurrState.ForEachClaim(func(miner address.Address, claim power.Claim) error {
+			claimModel = append(claimModel, &powermodel.PowerActorClaim{
+				Height:          int64(ec.CurrTs.Height()),
+				StateRoot:       ec.CurrTs.ParentState().String(),
+				MinerID:         miner.String(),
+				RawBytePower:    claim.RawBytePower.String(),
+				QualityAdjPower: claim.QualityAdjPower.String(),
+			})
+			return nil
+		}); err != nil {
+			return nil, err
+		}
+		return claimModel, nil
+	}
+	// normal case.
+	claimChanges, err := power.DiffClaims(ec.PrevState, ec.CurrState)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, newClaim := range claimChanges.Added {
+		claimModel = append(claimModel, &powermodel.PowerActorClaim{
+			Height:          int64(ec.CurrTs.Height()),
+			StateRoot:       ec.CurrTs.ParentState().String(),
+			MinerID:         newClaim.Miner.String(),
+			RawBytePower:    newClaim.Claim.RawBytePower.String(),
+			QualityAdjPower: newClaim.Claim.QualityAdjPower.String(),
+		})
+	}
+	for _, modClaim := range claimChanges.Modified {
+		claimModel = append(claimModel, &powermodel.PowerActorClaim{
+			Height:          int64(ec.CurrTs.Height()),
+			StateRoot:       ec.CurrTs.ParentState().String(),
+			MinerID:         modClaim.Miner.String(),
+			RawBytePower:    modClaim.To.RawBytePower.String(),
+			QualityAdjPower: modClaim.To.QualityAdjPower.String(),
+		})
+	}
+	return claimModel, nil
 }