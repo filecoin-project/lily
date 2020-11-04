adf92fb522f6d2d617ae19763e3a859577559cff
Cory Schwartz <cory@protocol.ai>
2020-11-03 22:22:31 -0800

    dont use branch name when tag is set

--- scripts/push-docker-tags.sh
+++ scripts/push-docker-tags.sh
@@ -57,7 +57,7 @@ pushTag () {
 if [ -z "$GIT_TAG" ]; then
 	pushTag "$GIT_BRANCH-${DATE_SHORT}-${GIT_SHA1_SHORT}"
 else
-	pushTag "$GIT_BRANCH-$GIT_TAG"
+	pushTag "$GIT_TAG"
 fi
 
 pushTag "$GIT_BRANCH-latest"
