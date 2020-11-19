807e651b63862118d1067121bda71cdf88d4be52
Mike Greenberg <mike.greenberg@protocol.ai>
2020-11-18 19:31:29 -0500

    fix: Correct chainvis views whose schema has shifted

--- storage/migrations/19_remove_unused_tables.go
+++ storage/migrations/19_remove_unused_tables.go
@@ -8,6 +8,34 @@ import (
 
 func init() {
 	up := batch(`
+DROP VIEW IF EXISTS chain_visualizer_chain_data_view;
+CREATE VIEW chain_visualizer_chain_data_view AS
+	SELECT
+		main_block.cid AS block,
+		bp.parent AS parent,
+		main_block.miner,
+		main_block.height,
+		main_block.parent_weight AS parentweight,
+		main_block.timestamp,
+		main_block.parent_state_root AS parentstateroot,
+		parent_block.timestamp AS parenttimestamp,
+		parent_block.height AS parentheight,
+		-- was miner_power.raw_bytes_power (plural bytes)
+		pac.raw_byte_power AS parentpower,
+		-- was blocks_synced.synced_at
+		main_block.timestamp AS syncedtimestamp,
+		(SELECT COUNT(*) FROM block_messages WHERE block_messages.block = main_block.cid) AS messages
+	FROM
+		block_headers main_block
+	LEFT JOIN
+		block_parents bp ON bp.block = main_block.cid
+	LEFT JOIN
+		block_headers parent_block ON parent_block.cid = bp.parent
+	LEFT JOIN
+		-- was miner_power (singular)
+		power_actor_claims pac ON main_block.parent_state_root = pac.state_root
+;
+
 	DROP MATERIALIZED VIEW IF EXISTS public.top_miners_by_base_reward; 				-- artifact from chainwatch
 	DROP MATERIALIZED VIEW IF EXISTS public.top_miners_by_base_reward_max_height;	-- artifact from chainwatch
 
