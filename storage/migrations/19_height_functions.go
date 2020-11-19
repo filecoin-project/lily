4291fb88ce995ae05f96509f160690b298dc1d42
Oli Evans <oli@tableflip.io>
2020-11-19 22:31:13 +0000

    feat: functions to convert unix epoch to fil epoch
    - adds `unix_to_height` and `height_and_unix` to translate a unix epoch in seconds to a fil epoch
    
    This is mainnet specific, so this may not be the right approach. I'd need to be able to programmatically set the initial unix epoch time to suport other networks
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

---
+++ storage/migrations/19_height_functions.go
@@ -0,0 +1,25 @@
+package migrations
+
+import (
+	"github.com/go-pg/migrations/v8"
+)
+
+// Schema version 19 adds helper functions to convert from unix epoch to fil epoch
+
+func init() {
+	up := batch(`
+CREATE OR REPLACE FUNCTION public.unix_to_height(unix_epoch bigint) RETURNS bigint AS $$
+	SELECT ((unix_epoch - 1598306400) / 30)::bigint;
+$$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION public.height_to_unix(fil_epoch bigint) RETURNS bigint AS $$
+	SELECT ((fil_epoch * 30) + 1598306400)::bigint;
+$$ LANGUAGE SQL;
+`)
+
+	down := batch(`
+	DROP FUNCTION IF EXISTS public.actor_tips;
+`)
+
+	migrations.MustRegisterTx(up, down)
+}
