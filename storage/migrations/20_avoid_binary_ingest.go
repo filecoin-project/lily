0cf9ff32abdab195fdfdb5fc4f33d5666109769e
Mike Greenberg <mike.greenberg@protocol.ai>
2020-11-19 18:05:32 -0500

    fix: Reorder migration to merge to master

--- storage/migrations/20_avoid_binary_ingest.go
+++ storage/migrations/20_avoid_binary_ingest.go
@@ -4,19 +4,20 @@ import (
 	"github.com/go-pg/migrations/v8"
 )
 
-// Schema version 19 removes unused binary data
+// Schema version 20 removes unused binary data
 
 func init() {
 	up := batch(`
 	DROP TABLE IF EXISTS public.drand_entries;
 
 	-- view depends on ticket and election_proof
-	DROP MATERIALIZED VIEW IF EXISTS chain_visualizer_blocks_view;
+	DROP VIEW IF EXISTS chain_visualizer_blocks_view;
 	ALTER TABLE public.block_headers DROP COLUMN ticket;
 	ALTER TABLE public.block_headers DROP COLUMN election_proof;
-	CREATE MATERIALIZED VIEW IF NOT EXISTS chain_visualizer_blocks_view AS
+
+	CREATE VIEW chain_visualizer_blocks_view AS
 		SELECT * FROM block_headers
-	WITH NO DATA;
+	;
 
 	ALTER TABLE public.messages DROP COLUMN params;
 
@@ -30,12 +31,12 @@ func init() {
 	);
 
 	-- view depends on ticket and election_proof
-	DROP MATERIALIZED VIEW IF EXISTS chain_visualizer_blocks_view;
+	DROP VIEW IF EXISTS chain_visualizer_blocks_view;
 	ALTER TABLE public.block_headers ADD COLUMN ticket bytea;
 	ALTER TABLE public.block_headers ADD COLUMN election_proof bytea;
-	CREATE MATERIALIZED VIEW IF NOT EXISTS chain_visualizer_blocks_view AS
+	CREATE VIEW chain_visualizer_blocks_view AS
 		SELECT * FROM block_headers
-	WITH NO DATA;
+	;
 
 	ALTER TABLE public.messages ADD COLUMN params bytea;
 
