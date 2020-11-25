6785051ac4dbcab164ed8fff9fa9c991ac10cda6
frrist <forrest@protocol.ai>
2020-11-25 13:29:55 -0800

    call cancel fun on all code paths

--- lens/carrepo/proxystore.go
+++ lens/carrepo/proxystore.go
@@ -70,6 +70,7 @@ func (pb *proxyingBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, 
 	cctx, cncl := context.WithCancel(ctx)
 	akc, err := pb.cache.AllKeysChan(cctx)
 	if err != nil {
+		cncl()
 		return nil, err
 	}
 	akc2, err2 := pb.store.AllKeysChan(cctx)
