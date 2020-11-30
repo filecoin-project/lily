4b9416c4b4a992b3b410c88d8c721d28b8bef020
Alan Shaw <alan.shaw@protocol.ai>
2020-11-30 20:31:05 +0000

    refactor: register for multiple actors versions

--- tasks/actorstate/account.go
+++ tasks/actorstate/account.go
@@ -4,14 +4,16 @@ import (
 	"context"
 
 	"github.com/filecoin-project/sentinel-visor/model"
-	builtin "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	sa0builtin "github.com/filecoin-project/specs-actors/actors/builtin"
+	sa2builtin "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
 // AccountExtractor is a state extractor that deals with Account actors.
 type AccountExtractor struct{}
 
 func init() {
-	Register(builtin.AccountActorCodeID, AccountExtractor{})
+	Register(sa0builtin.AccountActorCodeID, AccountExtractor{})
+	Register(sa2builtin.AccountActorCodeID, AccountExtractor{})
 }
 
 // Extract will create persistable data for a given actor's state.
