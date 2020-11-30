eb21ec6bc6279ee80d9cac3f317ae87cdc1b4870
Alan Shaw <alan.shaw@protocol.ai>
2020-11-30 19:49:44 +0000

    refactor: just collect all basic state

--- tasks/actorstate/account.go
+++ tasks/actorstate/account.go
@@ -3,7 +3,6 @@ package actorstate
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/sentinel-visor/model"
 	builtin "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
@@ -15,22 +14,9 @@ func init() {
 	Register(builtin.AccountActorCodeID, AccountExtractor{})
 }
 
-var includeAddrs = []address.Address{builtin.BurntFundsActorAddr}
-
 // Extract will create persistable data for a given actor's state.
 func (AccountExtractor) Extract(ctx context.Context, a ActorInfo, node ActorStateAPI) (model.Persistable, error) {
 	return model.NoData, nil
 }
 
-// Filter determines which actors this extractor is willing to extract.
-func (AccountExtractor) Filter(info ActorInfo) bool {
-	for _, a := range includeAddrs {
-		if a == info.Address {
-			return true
-		}
-	}
-	return false
-}
-
 var _ ActorStateExtractor = (*AccountExtractor)(nil)
-var _ FilteredExtractor = (*AccountExtractor)(nil)
--- tasks/actorstate/actorstate.go
+++ tasks/actorstate/actorstate.go
@@ -68,11 +68,6 @@ type ActorStateExtractor interface {
 	Extract(ctx context.Context, a ActorInfo, node ActorStateAPI) (model.Persistable, error)
 }
 
-// FilteredExtractor provides functionality to determine which actors should be extracted.
-type FilteredExtractor interface {
-	Filter(a ActorInfo) bool
-}
-
 // All supported actor state extractors
 var (
 	extractorsMu sync.Mutex
@@ -305,19 +300,6 @@ func (p *ActorStateProcessor) processActor(ctx context.Context, node lens.API, i
 	ctx, span := global.Tracer("").Start(ctx, "ActorStateProcessor.processActor")
 	defer span.End()
 
-	// Find the extractor for the actor type
-	extractor, exists := p.extractors[info.Actor.Code]
-	if !exists {
-		return xerrors.Errorf("no extractor defined for actor code %q", info.Actor.Code.String())
-	}
-
-	// Filter out this particular actor?
-	if fe, ok := extractor.(FilteredExtractor); ok {
-		if !fe.Filter(info) {
-			return nil
-		}
-	}
-
 	var ae ActorExtractor
 
 	// Persist the raw state
@@ -327,6 +309,12 @@ func (p *ActorStateProcessor) processActor(ctx context.Context, node lens.API, i
 	}
 	if err := data.Persist(ctx, p.storage.DB); err != nil {
 		return xerrors.Errorf("persisting raw state: %w", err)
+	}
+
+	// Find a specific extractor for the actor type
+	extractor, exists := p.extractors[info.Actor.Code]
+	if !exists {
+		return xerrors.Errorf("no extractor defined for actor code %q", info.Actor.Code.String())
 	}
 
 	ctx, _ = tag.New(ctx, tag.Upsert(metrics.TaskType, ActorNameByCode(info.Actor.Code)))
