8eca6ed4f85748d1adb817afbf4ff2b37a80ffc0
Alan Shaw <alan.shaw@protocol.ai>
2020-12-02 16:21:54 +0000

    feat: extract basic account actor states (#278)
    * feat: extract filtered account actors
    
    * feat: do no extract if filtered
    
    * fix: wording
    
    * refactor: just collect all basic state
    
    * refactor: register for multiple actors versions

---
+++ tasks/actorstate/account.go
@@ -0,0 +1,24 @@
+package actorstate
+
+import (
+	"context"
+
+	"github.com/filecoin-project/sentinel-visor/model"
+	sa0builtin "github.com/filecoin-project/specs-actors/actors/builtin"
+	sa2builtin "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+)
+
+// AccountExtractor is a state extractor that deals with Account actors.
+type AccountExtractor struct{}
+
+func init() {
+	Register(sa0builtin.AccountActorCodeID, AccountExtractor{})
+	Register(sa2builtin.AccountActorCodeID, AccountExtractor{})
+}
+
+// Extract will create persistable data for a given actor's state.
+func (AccountExtractor) Extract(ctx context.Context, a ActorInfo, node ActorStateAPI) (model.Persistable, error) {
+	return model.NoData, nil
+}
+
+var _ ActorStateExtractor = (*AccountExtractor)(nil)
