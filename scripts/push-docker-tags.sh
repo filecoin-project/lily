625a7cce948051f10df490230d473326a57f5368
Mike Greenberg <mike.greenberg@protocol.ai>
2021-02-18 06:06:07 -0500

    chore: make default docker image name deterministic; adjust ci build (#390)

--- scripts/push-docker-tags.sh
+++ scripts/push-docker-tags.sh
@@ -9,37 +9,35 @@
 # what tag, if any, to push to dockerhub.
 #
 # Usage:
-#   ./push-docker-tags.sh <image name> <git commit sha1> <git branch name> [git tag name] [dry run]
+#   ./push-docker-tags.sh <image name> <git commit sha1> [git tag name] [dry run]
 #
 # Example:
 #   # dry run. pass a 5th arg to have it print what it would do rather than do it.
-#   ./push-docker-tags.sh myiamge testingsha master "" dryrun
+#   ./push-docker-tags.sh myiamge testingsha "" dryrun
 #
 #   # push tag for commit on the main branch
-#   ./push-docker-tags.sh myimage testingsha main
+#   ./push-docker-tags.sh myimage testingsha
 #
 #   # push tag for a new release tag
-#   ./push-docker-tags.sh myimage testingsha release v0.5.0
+#   ./push-docker-tags.sh myimage testingsha v0.5.0
 #
 #   # serving suggestion in circle ci - https://circleci.com/docs/2.0/env-vars/#built-in-environment-variables
-#   ./push-docker-tags.sh filecoin/sentinel-visor $CIRCLE_SHA1 $CIRCLE_BRANCH $CIRCLE_TAG
+#   ./push-docker-tags.sh filecoin/sentinel-visor $CIRCLE_SHA1 $CIRCLE_TAG
 #
 set -euo pipefail
 
-if [[ $# -lt 3 ]] ; then
-  echo 'At least 3 args required. Pass 5 args for a dry run.'
+if [[ $# -lt 2 ]] ; then
+  echo 'At least 2 args required. Pass 4 args for a dry run.'
   echo 'Usage:'
-  echo './push-docker-tags.sh <image name> <git commit sha1> <git branch name> [git tag name] [dry run]'
+  echo './push-docker-tags.sh <image name> <git commit sha1> [git tag name] [dry run]'
   exit 1
 fi
 
 IMAGE_NAME=$1
 GIT_SHA1=$2
-GIT_SHA1_SHORT=$(echo "$GIT_SHA1" | cut -c 1-7)
-GIT_BRANCH=$3
-GIT_TAG=${4:-""}
-DRY_RUN=${5:-false}
-DATE_SHORT=$(date -u +%F)
+GIT_SHA1_SHORT=$(echo "$GIT_SHA1" | cut -c 1-8)
+GIT_TAG=${3:-""}
+DRY_RUN=${4:-false}
 
 pushTag () {
   local IMAGE_TAG="${1/\//-}"
@@ -54,8 +52,8 @@ pushTag () {
   fi
 }
 
-if [ -z "$GIT_TAG" ]; then
-	pushTag "$GIT_BRANCH-${DATE_SHORT}-${GIT_SHA1_SHORT}"
+if [ -z "${GIT_TAG}" ]; then
+	pushTag "${GIT_BRANCH}-${GIT_SHA1_SHORT}"
 else
-	pushTag "$GIT_TAG"
+	pushTag "${GIT_TAG}"
 fi
