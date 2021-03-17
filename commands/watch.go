a7b59a7b699ff08d6d54f8a6ca4588ebaedc523d
Ian Davis <nospam@iandavis.com>
2021-03-17 14:46:57 +0000

    chore: reuse watcher in lily watch command (#422)

--- commands/watch.go
+++ commands/watch.go
@@ -4,14 +4,19 @@ import (
 	"context"
 	"errors"
 	"strings"
+	"sync"
 	"time"
 
+	store "github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/sentinel-visor/schedule"
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/urfave/cli/v2"
+	"go.opencensus.io/stats"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/sentinel-visor/chain"
+	"github.com/filecoin-project/sentinel-visor/lens"
+	"github.com/filecoin-project/sentinel-visor/metrics"
 	"github.com/filecoin-project/sentinel-visor/model"
 	"github.com/filecoin-project/sentinel-visor/storage"
 )
@@ -77,10 +82,22 @@ func watch(cctx *cli.Context) error {
 		return xerrors.Errorf("setup indexer: %w", err)
 	}
 
+	notifier := NewLotusChainNotifier(lensOpener)
+
 	scheduler := schedule.NewScheduler(cctx.Duration("task-delay"))
+
+	// Subscribe to chain head events to be passed to the watcher
+	scheduler.Add(schedule.TaskConfig{
+		Name:                "ChainHeadNotifier",
+		Task:                notifier,
+		RestartOnFailure:    true,
+		RestartOnCompletion: true, // we always want the notifier to be running
+		RestartDelay:        time.Minute,
+	})
+
 	scheduler.Add(schedule.TaskConfig{
 		Name: "Watcher",
-		Task: chain.NewWatcher(tsIndexer, lensOpener, cctx.Int("indexhead-confidence")),
+		Task: chain.NewWatcher(tsIndexer, notifier, cctx.Int("indexhead-confidence")),
 		// TODO: add locker
 		// Locker:              NewGlobalSingleton(ChainHeadIndexerLockID, rctx.db), // only want one forward indexer anywhere to be running
 		RestartOnFailure:    true,
@@ -94,4 +111,106 @@ func watch(cctx *cli.Context) error {
 		return err
 	}
 	return nil
+}
+
+// LotusChainNotifier is a head event notifier that subscribes to a lens's ChainNotify method and adapts the
+// events received for use by a chain.Watcher
+// NOTE: this functionality will be probably folded into the Lotus API lens since other lenses will support more
+// direct methods of accessing new tipsets
+type LotusChainNotifier struct {
+	opener lens.APIOpener
+
+	mu     sync.Mutex            // protects following fields
+	events chan *chain.HeadEvent // created lazily, closed by first cancel call
+	err    error                 // set to non-nil by the first cancel call
+}
+
+func NewLotusChainNotifier(opener lens.APIOpener) *LotusChainNotifier {
+	return &LotusChainNotifier{
+		opener: opener,
+	}
+}
+
+func (c *LotusChainNotifier) eventsCh() chan *chain.HeadEvent {
+	// caller must hold mu
+	if c.events == nil {
+		c.events = make(chan *chain.HeadEvent)
+	}
+	return c.events
+}
+
+func (c *LotusChainNotifier) HeadEvents() <-chan *chain.HeadEvent {
+	c.mu.Lock()
+	ev := c.eventsCh()
+	c.mu.Unlock()
+	return ev
+}
+
+func (c *LotusChainNotifier) Err() error {
+	c.mu.Lock()
+	err := c.err
+	c.mu.Unlock()
+	return err
+}
+
+func (c *LotusChainNotifier) Cancel(err error) {
+	c.mu.Lock()
+	if c.err != nil {
+		c.mu.Unlock()
+		return
+	}
+	c.err = err
+
+	// ensure channel is closed even if it was never previously initialised
+	if c.events == nil {
+		c.events = make(chan *chain.HeadEvent)
+	}
+	close(c.events)
+	c.mu.Unlock()
+}
+
+// Run subscribes to ChainNotify and blocks until the context is done or
+// an error occurs.
+func (c *LotusChainNotifier) Run(ctx context.Context) error {
+	node, closer, err := c.opener.Open(ctx)
+	if err != nil {
+		return xerrors.Errorf("open lens: %w", err)
+	}
+	defer closer()
+
+	hc, err := node.ChainNotify(ctx)
+	if err != nil {
+		return xerrors.Errorf("chain notify: %w", err)
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			c.Cancel(ctx.Err())
+			return nil
+		case headEvents, ok := <-hc:
+			if !ok {
+				c.Cancel(xerrors.Errorf("ChainNotify channel closed"))
+				return nil
+			}
+
+			for _, ch := range headEvents {
+				stats.Record(ctx, metrics.WatchHeight.M(int64(ch.Val.Height())))
+				he := &chain.HeadEvent{
+					TipSet: ch.Val,
+				}
+				switch ch.Type {
+				case store.HCCurrent:
+					he.Type = chain.HeadEventCurrent
+				case store.HCApply:
+					he.Type = chain.HeadEventApply
+				case store.HCRevert:
+					he.Type = chain.HeadEventRevert
+				}
+
+				c.events <- he
+			}
+
+		}
+	}
 }
