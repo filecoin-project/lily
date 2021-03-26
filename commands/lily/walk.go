62fc75b1baad1445f89994e0f1a3312ed1050b25
Ian Davis <nospam@iandavis.com>
2021-03-26 14:16:49 +0000

    chore: remove confidence argument from lily walk command

--- commands/lily/walk.go
+++ commands/lily/walk.go
@@ -31,13 +31,6 @@ var LilyWalkCmd = &cli.Command{
 	Before: initialize,
 	After:  destroy,
 	Flags: []cli.Flag{
-		&cli.IntFlag{
-			Name:        "indexhead-confidence",
-			Usage:       "Sets the size of the cache used to hold tipsets for possible reversion before being committed to the database",
-			Value:       2,
-			EnvVars:     []string{"SENTINEL_LILY_WALK_CONFIDENCE"},
-			Destination: &walkFlags.confidence,
-		},
 		&cli.StringFlag{
 			Name:        "tasks",
 			Usage:       "Comma separated list of tasks to run. Each task is reported separately in the database.",
@@ -48,7 +41,7 @@ var LilyWalkCmd = &cli.Command{
 		&cli.DurationFlag{
 			Name:        "window",
 			Usage:       "Duration after which any indexing work not completed will be marked incomplete",
-			Value:       builtin.EpochDurationSeconds * time.Second,
+			Value:       builtin.EpochDurationSeconds * time.Second * 10, // walks don't need to complete within a single epoch
 			EnvVars:     []string{"SENTINEL_LILY_WALK_WINDOW"},
 			Destination: &walkFlags.window,
 		},
@@ -74,7 +67,6 @@ var LilyWalkCmd = &cli.Command{
 			Name:                "lily",
 			Tasks:               strings.Split(walkFlags.tasks, ","),
 			Window:              walkFlags.window,
-			Confidence:          walkFlags.confidence,
 			From:                walkFlags.from,
 			To:                  walkFlags.to,
 			RestartDelay:        0,
