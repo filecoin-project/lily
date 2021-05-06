filecoin-project/lily

commit 6f9f9998e6094d35cbb81796d5f4259b9b594971

fix(task): message task tracks unexecuted messages and their blocks (#491)
- fixes #426

chain/indexer.go
@@ -216,12 +216,12 @@ func (t *TipSetIndexer) TipSet(ctx context.Context, ts *types.TipSet) error {
 
 			// If we have message processors then extract the messages and receipts
 			if len(t.messageProcessors) > 0 {
-				emsgs, err := t.node.GetExecutedMessagesForTipset(ctx, child, parent)
+				emsgs, blkMsgs, err := t.node.GetExecutedAndBlockMessagesForTipset(ctx, child, parent)
 				if err == nil {
 					// Start all the message processors
 					for name, p := range t.messageProcessors {
 						inFlight++
-						go t.runMessageProcessor(tctx, p, name, child, parent, emsgs, results)
+						go t.runMessageProcessor(tctx, p, name, child, parent, emsgs, blkMsgs, results)
 					}
 				} else {
 					ll.Errorw("failed to extract messages", "error", err)
@@ -400,13 +400,13 @@ func (t *TipSetIndexer) runProcessor(ctx context.Context, p TipSetProcessor, nam
 	}
 }
 
-func (t *TipSetIndexer) runMessageProcessor(ctx context.Context, p MessageProcessor, name string, ts, pts *types.TipSet, emsgs []*lens.ExecutedMessage, results chan *TaskResult) {
+func (t *TipSetIndexer) runMessageProcessor(ctx context.Context, p MessageProcessor, name string, ts, pts *types.TipSet, emsgs []*lens.ExecutedMessage, blkMsgs []*lens.BlockMessages, results chan *TaskResult) {
 	ctx, _ = tag.New(ctx, tag.Upsert(metrics.TaskType, name))
 	stats.Record(ctx, metrics.TipsetHeight.M(int64(ts.Height())))
 	stop := metrics.Timer(ctx, metrics.ProcessingDuration)
 	defer stop()
 
-	data, report, err := p.ProcessMessages(ctx, ts, pts, emsgs)
+	data, report, err := p.ProcessMessages(ctx, ts, pts, emsgs, blkMsgs)
 	if err != nil {
 		stats.Record(ctx, metrics.ProcessingFailure.M(1))
 		results <- &TaskResult{
@@ -514,7 +514,7 @@ type MessageProcessor interface {
 	// ProcessMessages processes messages contained within a tipset. If error is non-nil then the processor encountered a fatal error.
 	// pts is the tipset containing the messages, ts is the tipset containing the receipts
 	// Any data returned must be accompanied by a processing report.
-	ProcessMessages(ctx context.Context, ts *types.TipSet, pts *types.TipSet, emsgs []*lens.ExecutedMessage) (model.Persistable, *visormodel.ProcessingReport, error)
+	ProcessMessages(ctx context.Context, ts *types.TipSet, pts *types.TipSet, emsgs []*lens.ExecutedMessage, blkMsgs []*lens.BlockMessages) (model.Persistable, *visormodel.ProcessingReport, error)
 	Close() error
 }
 

lens/interface.go
@@ -21,7 +21,7 @@ type API interface {
 	ChainAPI
 	StateAPI
 
-	GetExecutedMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*ExecutedMessage, error)
+	GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*ExecutedMessage, []*BlockMessages, error)
 }
 
 type StoreAPI interface {
@@ -78,4 +78,10 @@ type ExecutedMessage struct {
 	FromActorCode cid.Cid   // code of the actor the message is from
 	ToActorCode   cid.Cid   // code of the actor the message is to
 	GasOutputs    vm.GasOutputs
+}
+
+type BlockMessages struct {
+	Block        *types.BlockHeader     // block messages appeared in
+	BlsMessages  []*types.Message       // BLS messages in block `Block`
+	SecpMessages []*types.SignedMessage // SECP messages in block `Block`
 }

lens/lily/impl.go
@@ -129,8 +129,8 @@ func (m *LilyNodeAPI) Open(_ context.Context) (lens.API, lens.APICloser, error) 
 	return m, func() {}, nil
 }
 
-func (m *LilyNodeAPI) GetExecutedMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, error) {
-	return util.GetExecutedMessagesForTipset(ctx, m.ChainAPI.Chain, ts, pts)
+func (m *LilyNodeAPI) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+	return util.GetExecutedAndBlockMessagesForTipset(ctx, m.ChainAPI.Chain, ts, pts)
 }
 
 func (m *LilyNodeAPI) Store() adt.Store {

lens/lily/struct.go
@@ -23,8 +23,8 @@ type LilyAPIStruct struct {
 	v0api.FullNodeStruct
 
 	Internal struct {
-		Store                        func() adt.Store                                                                     `perm:"read"`
-		GetExecutedMessagesForTipset func(context.Context, *types.TipSet, *types.TipSet) ([]*lens.ExecutedMessage, error) `perm:"read"`
+		Store                                func() adt.Store                                                                                            `perm:"read"`
+		GetExecutedAndBlockMessagesForTipset func(context.Context, *types.TipSet, *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) `perm:"read"`
 
 		LilyWatch func(context.Context, *LilyWatchConfig) (schedule.JobID, error) `perm:"read"`
 		LilyWalk  func(context.Context, *LilyWalkConfig) (schedule.JobID, error)  `perm:"read"`
@@ -59,8 +59,8 @@ func (s *LilyAPIStruct) LilyJobList(ctx context.Context) ([]schedule.JobResult, 
 	return s.Internal.LilyJobList(ctx)
 }
 
-func (s *LilyAPIStruct) GetExecutedMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, error) {
-	return s.Internal.GetExecutedMessagesForTipset(ctx, ts, pts)
+func (s *LilyAPIStruct) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+	return s.Internal.GetExecutedAndBlockMessagesForTipset(ctx, ts, pts)
 }
 
 var _ LilyAPI = &LilyAPIStruct{}

lens/lotus/api.go
@@ -200,35 +200,36 @@ func (aw *APIWrapper) StateVMCirculatingSupplyInternal(ctx context.Context, tsk 
 	return aw.FullNode.StateVMCirculatingSupplyInternal(ctx, tsk)
 }
 
-// GetExecutedMessagesForTipset returns a list of messages sent as part of pts (parent) with receipts found in ts (child).
-// No attempt at deduplication of messages is made.
-func (aw *APIWrapper) GetExecutedMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, error) {
-	ctx, _ = tag.New(ctx, tag.Upsert(metrics.API, "GetExecutedMessagesForTipset"))
+// GetExecutedAndBlockMessagesForTipset returns a list of messages sent as part of pts (parent) with receipts found in ts (child).
+// No attempt at deduplication of messages is made. A list of blocks with their corresponding messages is also returned - it contains all messages
+// in the block regardless if they were applied during the state change.
+func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+	ctx, _ = tag.New(ctx, tag.Upsert(metrics.API, "GetExecutedAndBlockMessagesForTipset"))
 	stop := metrics.Timer(ctx, metrics.LensRequestDuration)
 	defer stop()
 
 	if !types.CidArrsEqual(ts.Parents().Cids(), pts.Cids()) {
-		return nil, xerrors.Errorf("child tipset (%s) is not on the same chain as parent (%s)", ts.Key(), pts.Key())
+		return nil, nil, xerrors.Errorf("child tipset (%s) is not on the same chain as parent (%s)", ts.Key(), pts.Key())
 	}
 
 	stateTree, err := state.LoadStateTree(aw.Store(), ts.ParentState())
 	if err != nil {
-		return nil, xerrors.Errorf("load state tree: %w", err)
+		return nil, nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
 	parentStateTree, err := state.LoadStateTree(aw.Store(), pts.ParentState())
 	if err != nil {
-		return nil, xerrors.Errorf("load parent state tree: %w", err)
+		return nil, nil, xerrors.Errorf("load parent state tree: %w", err)
 	}
 
 	initActor, err := stateTree.GetActor(builtininit.Address)
 	if err != nil {
-		return nil, xerrors.Errorf("getting init actor: %w", err)
+		return nil, nil, xerrors.Errorf("getting init actor: %w", err)
 	}
 
 	initActorState, err := builtininit.Load(aw.Store(), initActor)
 	if err != nil {
-		return nil, xerrors.Errorf("loading init actor state: %w", err)
+		return nil, nil, xerrors.Errorf("loading init actor state: %w", err)
 	}
 
 	// Build a lookup of actor codes
@@ -237,7 +238,7 @@ func (aw *APIWrapper) GetExecutedMessagesForTipset(ctx context.Context, ts, pts 
 		actorCodes[a] = act.Code
 		return nil
 	}); err != nil {
-		return nil, xerrors.Errorf("iterate actors: %w", err)
+		return nil, nil, xerrors.Errorf("iterate actors: %w", err)
 	}
 
 	getActorCode := func(a address.Address) cid.Cid {
@@ -266,7 +267,7 @@ func (aw *APIWrapper) GetExecutedMessagesForTipset(ctx context.Context, ts, pts 
 	for _, blkCid := range pts.Cids() {
 		blkMsgs, err := aw.ChainGetBlockMessages(ctx, blkCid)
 		if err != nil {
-			return nil, xerrors.Errorf("get block messages: %w", err)
+			return nil, nil, xerrors.Errorf("get block messages: %w", err)
 		}
 
 		for _, mcid := range blkMsgs.Cids {
@@ -277,18 +278,18 @@ func (aw *APIWrapper) GetExecutedMessagesForTipset(ctx context.Context, ts, pts 
 	// Get messages that were processed in the parent tipset
 	msgs, err := aw.ChainGetParentMessages(ctx, ts.Cids()[0])
 	if err != nil {
-		return nil, xerrors.Errorf("get parent messages: %w", err)
+		return nil, nil, xerrors.Errorf("get parent messages: %w", err)
 	}
 
 	// Get receipts for parent messages
 	rcpts, err := aw.ChainGetParentReceipts(ctx, ts.Cids()[0])
 	if err != nil {
-		return nil, xerrors.Errorf("get parent receipts: %w", err)
+		return nil, nil, xerrors.Errorf("get parent receipts: %w", err)
 	}
 
 	if len(rcpts) != len(msgs) {
 		// logic error somewhere
-		return nil, xerrors.Errorf("mismatching number of receipts: got %d wanted %d", len(rcpts), len(msgs))
+		return nil, nil, xerrors.Errorf("mismatching number of receipts: got %d wanted %d", len(rcpts), len(msgs))
 	}
 
 	// Start building a list of completed message with receipt
@@ -301,7 +302,7 @@ func (aw *APIWrapper) GetExecutedMessagesForTipset(ctx context.Context, ts, pts 
 		Bstore:    &apiBlockstore{api: aw.FullNode}, // sadly vm wraps this to turn it back into an adt.Store
 	})
 	if err != nil {
-		return nil, xerrors.Errorf("creating temporary vm: %w", err)
+		return nil, nil, xerrors.Errorf("creating temporary vm: %w", err)
 	}
 
 	for index, m := range msgs {
@@ -320,14 +321,28 @@ func (aw *APIWrapper) GetExecutedMessagesForTipset(ctx context.Context, ts, pts 
 
 		burn, err := vmi.ShouldBurn(parentStateTree, m.Message, rcpts[index].ExitCode)
 		if err != nil {
-			return nil, xerrors.Errorf("deciding whether should burn failed: %w", err)
+			return nil, nil, xerrors.Errorf("deciding whether should burn failed: %w", err)
 		}
 
 		em.GasOutputs = vm.ComputeGasOutputs(em.Receipt.GasUsed, em.Message.GasLimit, em.BlockHeader.ParentBaseFee, em.Message.GasFeeCap, em.Message.GasPremium, burn)
 		emsgs = append(emsgs, em)
 	}
 
-	return emsgs, nil
+	blkMsgs := make([]*lens.BlockMessages, len(ts.Blocks()))
+	for idx, blk := range ts.Blocks() {
+		msgs, err := aw.ChainGetBlockMessages(ctx, blk.Cid())
+		if err != nil {
+			return nil, nil, err
+		}
+
+		blkMsgs[idx] = &lens.BlockMessages{
+			Block:        blk,
+			BlsMessages:  msgs.BlsMessages,
+			SecpMessages: msgs.SecpkMessages,
+		}
+	}
+
+	return emsgs, blkMsgs, nil
 }
 
 var _ blockstore.Blockstore = (*apiBlockstore)(nil)

lens/lotusrepo/repo.go
@@ -109,8 +109,8 @@ type RepoAPI struct {
 	cacheSize int
 }
 
-func (ra *RepoAPI) GetExecutedMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, error) {
-	return util.GetExecutedMessagesForTipset(ctx, ra.FullNodeAPI.ChainAPI.Chain, ts, pts)
+func (ra *RepoAPI) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+	return util.GetExecutedAndBlockMessagesForTipset(ctx, ra.FullNodeAPI.ChainAPI.Chain, ts, pts)
 }
 
 func (ra *RepoAPI) Store() adt.Store {

commit fdcb1b1b4675145a03faac60daf0ede9098f398c

fix(api): jsonrpc only supports returning 2 params
- fix bug introduced in #491

chain/indexer.go
@@ -216,12 +216,12 @@ func (t *TipSetIndexer) TipSet(ctx context.Context, ts *types.TipSet) error {
 
 			// If we have message processors then extract the messages and receipts
 			if len(t.messageProcessors) > 0 {
-				emsgs, blkMsgs, err := t.node.GetExecutedAndBlockMessagesForTipset(ctx, child, parent)
+				tsMsgs, err := t.node.GetExecutedAndBlockMessagesForTipset(ctx, child, parent)
 				if err == nil {
 					// Start all the message processors
 					for name, p := range t.messageProcessors {
 						inFlight++
-						go t.runMessageProcessor(tctx, p, name, child, parent, emsgs, blkMsgs, results)
+						go t.runMessageProcessor(tctx, p, name, child, parent, tsMsgs.Executed, tsMsgs.Block, results)
 					}
 				} else {
 					ll.Errorw("failed to extract messages", "error", err)

lens/interface.go
@@ -21,7 +21,7 @@ type API interface {
 	ChainAPI
 	StateAPI
 
-	GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*ExecutedMessage, []*BlockMessages, error)
+	GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) (*TipSetMessages, error)
 }
 
 type StoreAPI interface {
@@ -65,6 +65,11 @@ type APICloser func()
 
 type APIOpener interface {
 	Open(context.Context) (API, APICloser, error)
+}
+
+type TipSetMessages struct {
+	Executed []*ExecutedMessage
+	Block    []*BlockMessages
 }
 
 type ExecutedMessage struct {

lens/lily/impl.go
@@ -129,7 +129,7 @@ func (m *LilyNodeAPI) Open(_ context.Context) (lens.API, lens.APICloser, error) 
 	return m, func() {}, nil
 }
 
-func (m *LilyNodeAPI) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+func (m *LilyNodeAPI) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) (*lens.TipSetMessages, error) {
 	return util.GetExecutedAndBlockMessagesForTipset(ctx, m.ChainAPI.Chain, ts, pts)
 }
 

lens/lily/struct.go
@@ -23,8 +23,8 @@ type LilyAPIStruct struct {
 	v0api.FullNodeStruct
 
 	Internal struct {
-		Store                                func() adt.Store                                                                                            `perm:"read"`
-		GetExecutedAndBlockMessagesForTipset func(context.Context, *types.TipSet, *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) `perm:"read"`
+		Store                                func() adt.Store                                                                  `perm:"read"`
+		GetExecutedAndBlockMessagesForTipset func(context.Context, *types.TipSet, *types.TipSet) (*lens.TipSetMessages, error) `perm:"read"`
 
 		LilyWatch func(context.Context, *LilyWatchConfig) (schedule.JobID, error) `perm:"read"`
 		LilyWalk  func(context.Context, *LilyWalkConfig) (schedule.JobID, error)  `perm:"read"`
@@ -59,7 +59,7 @@ func (s *LilyAPIStruct) LilyJobList(ctx context.Context) ([]schedule.JobResult, 
 	return s.Internal.LilyJobList(ctx)
 }
 
-func (s *LilyAPIStruct) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+func (s *LilyAPIStruct) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) (*lens.TipSetMessages, error) {
 	return s.Internal.GetExecutedAndBlockMessagesForTipset(ctx, ts, pts)
 }
 

lens/lotus/api.go
@@ -203,33 +203,33 @@ func (aw *APIWrapper) StateVMCirculatingSupplyInternal(ctx context.Context, tsk 
 // GetExecutedAndBlockMessagesForTipset returns a list of messages sent as part of pts (parent) with receipts found in ts (child).
 // No attempt at deduplication of messages is made. A list of blocks with their corresponding messages is also returned - it contains all messages
 // in the block regardless if they were applied during the state change.
-func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) (*lens.TipSetMessages, error) {
 	ctx, _ = tag.New(ctx, tag.Upsert(metrics.API, "GetExecutedAndBlockMessagesForTipset"))
 	stop := metrics.Timer(ctx, metrics.LensRequestDuration)
 	defer stop()
 
 	if !types.CidArrsEqual(ts.Parents().Cids(), pts.Cids()) {
-		return nil, nil, xerrors.Errorf("child tipset (%s) is not on the same chain as parent (%s)", ts.Key(), pts.Key())
+		return nil, xerrors.Errorf("child tipset (%s) is not on the same chain as parent (%s)", ts.Key(), pts.Key())
 	}
 
 	stateTree, err := state.LoadStateTree(aw.Store(), ts.ParentState())
 	if err != nil {
-		return nil, nil, xerrors.Errorf("load state tree: %w", err)
+		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
 	parentStateTree, err := state.LoadStateTree(aw.Store(), pts.ParentState())
 	if err != nil {
-		return nil, nil, xerrors.Errorf("load parent state tree: %w", err)
+		return nil, xerrors.Errorf("load parent state tree: %w", err)
 	}
 
 	initActor, err := stateTree.GetActor(builtininit.Address)
 	if err != nil {
-		return nil, nil, xerrors.Errorf("getting init actor: %w", err)
+		return nil, xerrors.Errorf("getting init actor: %w", err)
 	}
 
 	initActorState, err := builtininit.Load(aw.Store(), initActor)
 	if err != nil {
-		return nil, nil, xerrors.Errorf("loading init actor state: %w", err)
+		return nil, xerrors.Errorf("loading init actor state: %w", err)
 	}
 
 	// Build a lookup of actor codes
@@ -238,7 +238,7 @@ func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, 
 		actorCodes[a] = act.Code
 		return nil
 	}); err != nil {
-		return nil, nil, xerrors.Errorf("iterate actors: %w", err)
+		return nil, xerrors.Errorf("iterate actors: %w", err)
 	}
 
 	getActorCode := func(a address.Address) cid.Cid {
@@ -267,7 +267,7 @@ func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, 
 	for _, blkCid := range pts.Cids() {
 		blkMsgs, err := aw.ChainGetBlockMessages(ctx, blkCid)
 		if err != nil {
-			return nil, nil, xerrors.Errorf("get block messages: %w", err)
+			return nil, xerrors.Errorf("get block messages: %w", err)
 		}
 
 		for _, mcid := range blkMsgs.Cids {
@@ -278,18 +278,18 @@ func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, 
 	// Get messages that were processed in the parent tipset
 	msgs, err := aw.ChainGetParentMessages(ctx, ts.Cids()[0])
 	if err != nil {
-		return nil, nil, xerrors.Errorf("get parent messages: %w", err)
+		return nil, xerrors.Errorf("get parent messages: %w", err)
 	}
 
 	// Get receipts for parent messages
 	rcpts, err := aw.ChainGetParentReceipts(ctx, ts.Cids()[0])
 	if err != nil {
-		return nil, nil, xerrors.Errorf("get parent receipts: %w", err)
+		return nil, xerrors.Errorf("get parent receipts: %w", err)
 	}
 
 	if len(rcpts) != len(msgs) {
 		// logic error somewhere
-		return nil, nil, xerrors.Errorf("mismatching number of receipts: got %d wanted %d", len(rcpts), len(msgs))
+		return nil, xerrors.Errorf("mismatching number of receipts: got %d wanted %d", len(rcpts), len(msgs))
 	}
 
 	// Start building a list of completed message with receipt
@@ -302,7 +302,7 @@ func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, 
 		Bstore:    &apiBlockstore{api: aw.FullNode}, // sadly vm wraps this to turn it back into an adt.Store
 	})
 	if err != nil {
-		return nil, nil, xerrors.Errorf("creating temporary vm: %w", err)
+		return nil, xerrors.Errorf("creating temporary vm: %w", err)
 	}
 
 	for index, m := range msgs {
@@ -321,7 +321,7 @@ func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, 
 
 		burn, err := vmi.ShouldBurn(parentStateTree, m.Message, rcpts[index].ExitCode)
 		if err != nil {
-			return nil, nil, xerrors.Errorf("deciding whether should burn failed: %w", err)
+			return nil, xerrors.Errorf("deciding whether should burn failed: %w", err)
 		}
 
 		em.GasOutputs = vm.ComputeGasOutputs(em.Receipt.GasUsed, em.Message.GasLimit, em.BlockHeader.ParentBaseFee, em.Message.GasFeeCap, em.Message.GasPremium, burn)
@@ -332,7 +332,7 @@ func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, 
 	for idx, blk := range ts.Blocks() {
 		msgs, err := aw.ChainGetBlockMessages(ctx, blk.Cid())
 		if err != nil {
-			return nil, nil, err
+			return nil, err
 		}
 
 		blkMsgs[idx] = &lens.BlockMessages{
@@ -342,7 +342,10 @@ func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, 
 		}
 	}
 
-	return emsgs, blkMsgs, nil
+	return &lens.TipSetMessages{
+		Executed: emsgs,
+		Block:    blkMsgs,
+	}, nil
 }
 
 var _ blockstore.Blockstore = (*apiBlockstore)(nil)

lens/lotusrepo/repo.go
@@ -109,7 +109,7 @@ type RepoAPI struct {
 	cacheSize int
 }
 
-func (ra *RepoAPI) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+func (ra *RepoAPI) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) (*lens.TipSetMessages, error) {
 	return util.GetExecutedAndBlockMessagesForTipset(ctx, ra.FullNodeAPI.ChainAPI.Chain, ts, pts)
 }
 

lens/util/repo.go
@@ -116,7 +116,7 @@ type LensAPI struct {
 	cs        *store.ChainStore
 }
 
-func (ra *LensAPI) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+func (ra *LensAPI) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) (*lens.TipSetMessages, error) {
 	return GetExecutedAndBlockMessagesForTipset(ctx, ra.cs, ts, pts)
 }
 
@@ -241,29 +241,29 @@ func (m fakeVerifier) GenerateWinningPoStSectorChallenge(ctx context.Context, pr
 // GetMessagesForTipset returns a list of messages sent as part of pts (parent) with receipts found in ts (child).
 // No attempt at deduplication of messages is made. A list of blocks with their corresponding messages is also returned - it contains all messages
 // in the block regardless if they were applied during the state change.
-func GetExecutedAndBlockMessagesForTipset(ctx context.Context, cs *store.ChainStore, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+func GetExecutedAndBlockMessagesForTipset(ctx context.Context, cs *store.ChainStore, ts, pts *types.TipSet) (*lens.TipSetMessages, error) {
 	if !types.CidArrsEqual(ts.Parents().Cids(), pts.Cids()) {
-		return nil, nil, xerrors.Errorf("child tipset (%s) is not on the same chain as parent (%s)", ts.Key(), pts.Key())
+		return nil, xerrors.Errorf("child tipset (%s) is not on the same chain as parent (%s)", ts.Key(), pts.Key())
 	}
 
 	stateTree, err := state.LoadStateTree(cs.ActorStore(ctx), ts.ParentState())
 	if err != nil {
-		return nil, nil, xerrors.Errorf("load state tree: %w", err)
+		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
 	parentStateTree, err := state.LoadStateTree(cs.ActorStore(ctx), pts.ParentState())
 	if err != nil {
-		return nil, nil, xerrors.Errorf("load state tree: %w", err)
+		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
 	initActor, err := stateTree.GetActor(builtininit.Address)
 	if err != nil {
-		return nil, nil, xerrors.Errorf("getting init actor: %w", err)
+		return nil, xerrors.Errorf("getting init actor: %w", err)
 	}
 
 	initActorState, err := builtininit.Load(cs.ActorStore(ctx), initActor)
 	if err != nil {
-		return nil, nil, xerrors.Errorf("loading init actor state: %w", err)
+		return nil, xerrors.Errorf("loading init actor state: %w", err)
 	}
 
 	// Build a lookup of actor codes
@@ -272,7 +272,7 @@ func GetExecutedAndBlockMessagesForTipset(ctx context.Context, cs *store.ChainSt
 		actorCodes[a] = act.Code
 		return nil
 	}); err != nil {
-		return nil, nil, xerrors.Errorf("iterate actors: %w", err)
+		return nil, xerrors.Errorf("iterate actors: %w", err)
 	}
 
 	getActorCode := func(a address.Address) cid.Cid {
@@ -294,7 +294,7 @@ func GetExecutedAndBlockMessagesForTipset(ctx context.Context, cs *store.ChainSt
 	for blockIdx, bh := range pts.Blocks() {
 		blscids, secpkcids, err := cs.ReadMsgMetaCids(bh.Messages)
 		if err != nil {
-			return nil, nil, xerrors.Errorf("read messages for block: %w", err)
+			return nil, xerrors.Errorf("read messages for block: %w", err)
 		}
 
 		for _, c := range blscids {
@@ -309,13 +309,13 @@ func GetExecutedAndBlockMessagesForTipset(ctx context.Context, cs *store.ChainSt
 
 	bmsgs, err := cs.BlockMsgsForTipset(pts)
 	if err != nil {
-		return nil, nil, xerrors.Errorf("block messages for tipset: %w", err)
+		return nil, xerrors.Errorf("block messages for tipset: %w", err)
 	}
 
 	pblocks := pts.Blocks()
 	if len(bmsgs) != len(pblocks) {
 		// logic error somewhere
-		return nil, nil, xerrors.Errorf("mismatching number of blocks returned from block messages, got %d wanted %d", len(bmsgs), len(pblocks))
+		return nil, xerrors.Errorf("mismatching number of blocks returned from block messages, got %d wanted %d", len(bmsgs), len(pblocks))
 	}
 
 	count := 0
@@ -364,12 +364,12 @@ func GetExecutedAndBlockMessagesForTipset(ctx context.Context, cs *store.ChainSt
 	// Retrieve receipts using a block from the child tipset
 	rs, err := adt.AsArray(cs.ActorStore(ctx), ts.Blocks()[0].ParentMessageReceipts)
 	if err != nil {
-		return nil, nil, xerrors.Errorf("amt load: %w", err)
+		return nil, xerrors.Errorf("amt load: %w", err)
 	}
 
 	if rs.Length() != uint64(len(emsgs)) {
 		// logic error somewhere
-		return nil, nil, xerrors.Errorf("mismatching number of receipts: got %d wanted %d", rs.Length(), len(emsgs))
+		return nil, xerrors.Errorf("mismatching number of receipts: got %d wanted %d", rs.Length(), len(emsgs))
 	}
 
 	// Create a skeleton vm just for calling ShouldBurn
@@ -379,22 +379,22 @@ func GetExecutedAndBlockMessagesForTipset(ctx context.Context, cs *store.ChainSt
 		Bstore:    cs.StateBlockstore(),
 	})
 	if err != nil {
-		return nil, nil, xerrors.Errorf("creating temporary vm: %w", err)
+		return nil, xerrors.Errorf("creating temporary vm: %w", err)
 	}
 
 	// Receipts are in same order as BlockMsgsForTipset
 	for _, em := range emsgs {
 		var r types.MessageReceipt
 		if found, err := rs.Get(em.Index, &r); err != nil {
-			return nil, nil, err
+			return nil, err
 		} else if !found {
-			return nil, nil, xerrors.Errorf("failed to find receipt %d", em.Index)
+			return nil, xerrors.Errorf("failed to find receipt %d", em.Index)
 		}
 		em.Receipt = &r
 
 		burn, err := vmi.ShouldBurn(parentStateTree, em.Message, em.Receipt.ExitCode)
 		if err != nil {
-			return nil, nil, xerrors.Errorf("deciding whether should burn failed: %w", err)
+			return nil, xerrors.Errorf("deciding whether should burn failed: %w", err)
 		}
 
 		em.GasOutputs = vm.ComputeGasOutputs(em.Receipt.GasUsed, em.Message.GasLimit, em.BlockHeader.ParentBaseFee, em.Message.GasFeeCap, em.Message.GasPremium, burn)
@@ -404,7 +404,7 @@ func GetExecutedAndBlockMessagesForTipset(ctx context.Context, cs *store.ChainSt
 	for idx, blk := range ts.Blocks() {
 		msgs, smsgs, err := cs.MessagesForBlock(blk)
 		if err != nil {
-			return nil, nil, err
+			return nil, err
 		}
 		blkMsgs[idx] = &lens.BlockMessages{
 			Block:        blk,
@@ -413,5 +413,8 @@ func GetExecutedAndBlockMessagesForTipset(ctx context.Context, cs *store.ChainSt
 		}
 	}
 
-	return emsgs, blkMsgs, nil
+	return &lens.TipSetMessages{
+		Executed: emsgs,
+		Block:    blkMsgs,
+	}, nil
 }

lens/vector/repo.go
@@ -146,7 +146,7 @@ func (c *CaptureAPI) Store() adt.Store {
 	return adtStore
 }
 
-func (c *CaptureAPI) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
+func (c *CaptureAPI) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) (*lens.TipSetMessages, error) {
 	return util.GetExecutedAndBlockMessagesForTipset(ctx, c.FullNodeAPI.ChainAPI.Chain, ts, pts)
 }
 

testutil/lens.go
@@ -39,8 +39,8 @@ func (aw *APIWrapper) Store() adt.Store {
 	return aw
 }
 
-func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) ([]*lens.ExecutedMessage, []*lens.BlockMessages, error) {
-	return nil, nil, xerrors.Errorf("GetExecutedAndBlockMessagesForTipset is not implemented")
+func (aw *APIWrapper) GetExecutedAndBlockMessagesForTipset(ctx context.Context, ts, pts *types.TipSet) (*lens.TipSetMessages, error) {
+	return nil, xerrors.Errorf("GetExecutedAndBlockMessagesForTipset is not implemented")
 }
 
 func (aw *APIWrapper) Get(ctx context.Context, c cid.Cid, out interface{}) error {