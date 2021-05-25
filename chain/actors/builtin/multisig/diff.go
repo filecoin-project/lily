420d6f89a3fd75a420d4b860e1081cd24054da15
frrist <forrest@protocol.ai>
2021-05-25 14:53:07 -0700

    perf(multisig): update transactions hamt diffing

--- chain/actors/builtin/multisig/diff.go
+++ chain/actors/builtin/multisig/diff.go
@@ -1,9 +1,15 @@
 package multisig
 
 import (
+	"context"
+
 	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-hamt-ipld/v3"
 	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/sentinel-visor/chain/actors/adt"
 	"github.com/filecoin-project/sentinel-visor/chain/actors/adt/diff"
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
@@ -24,14 +30,7 @@ type TransactionModification struct {
 	To   Transaction
 }
 
-func DiffPendingTransactions(pre, cur State) (*PendingTransactionChanges, error) {
-	results := new(PendingTransactionChanges)
-	if changed, err := pre.PendingTxnChanged(cur); err != nil {
-		return nil, err
-	} else if !changed { // if nothing has changed then return an empty result and bail.
-		return results, nil
-	}
-
+func DiffPendingTransactions(ctx context.Context, store adt.Store, pre, cur State) (*PendingTransactionChanges, error) {
 	pret, err := pre.transactions()
 	if err != nil {
 		return nil, err
@@ -42,18 +41,61 @@ func DiffPendingTransactions(pre, cur State) (*PendingTransactionChanges, error)
 		return nil, err
 	}
 
-	if err := diff.GenericMap(pret, curt, &transactionDiffer{results, pre, cur}); err != nil {
+	preOpts, err := adt.MapOptsForActorCode(pre.Code())
+	if err != nil {
 		return nil, err
 	}
-	return results, nil
+	curOpts, err := adt.MapOptsForActorCode(cur.Code())
+	if err != nil {
+		return nil, err
+	}
+	diffContainer := NewTransactionDiffContainer(pre, cur)
+	if requiresLegacyDiffing(pre, cur, preOpts, curOpts) {
+		if err := diff.GenericMap(pret, curt, diffContainer); err != nil {
+			return nil, err
+		}
+		return diffContainer.Results, nil
+	}
+
+	changes, err := diff.Hamt(ctx, pret, curt, store, store, hamt.UseTreeBitWidth(preOpts.Bitwidth), hamt.UseHashFunction(hamt.HashFunc(preOpts.HashFunc)))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, change := range changes {
+		switch change.Type {
+		case hamt.Add:
+			if err := diffContainer.Add(change.Key, change.After); err != nil {
+				return nil, err
+			}
+		case hamt.Remove:
+			if err := diffContainer.Remove(change.Key, change.Before); err != nil {
+				return nil, err
+			}
+		case hamt.Modify:
+			if err := diffContainer.Modify(change.Key, change.Before, change.After); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return diffContainer.Results, nil
 }
 
-type transactionDiffer struct {
+func NewTransactionDiffContainer(pre, cur State) *transactionDiffContainer {
+	return &transactionDiffContainer{
+		Results: new(PendingTransactionChanges),
+		pre:     pre,
+		after:   cur,
+	}
+}
+
+type transactionDiffContainer struct {
 	Results    *PendingTransactionChanges
 	pre, after State
 }
 
-func (t *transactionDiffer) AsKey(key string) (abi.Keyer, error) {
+func (t *transactionDiffContainer) AsKey(key string) (abi.Keyer, error) {
 	txID, err := abi.ParseIntKey(key)
 	if err != nil {
 		return nil, err
@@ -61,7 +103,7 @@ func (t *transactionDiffer) AsKey(key string) (abi.Keyer, error) {
 	return abi.IntKey(txID), nil
 }
 
-func (t *transactionDiffer) Add(key string, val *cbg.Deferred) error {
+func (t *transactionDiffContainer) Add(key string, val *cbg.Deferred) error {
 	txID, err := abi.ParseIntKey(key)
 	if err != nil {
 		return err
@@ -77,7 +119,7 @@ func (t *transactionDiffer) Add(key string, val *cbg.Deferred) error {
 	return nil
 }
 
-func (t *transactionDiffer) Modify(key string, from, to *cbg.Deferred) error {
+func (t *transactionDiffContainer) Modify(key string, from, to *cbg.Deferred) error {
 	txID, err := abi.ParseIntKey(key)
 	if err != nil {
 		return err
@@ -116,7 +158,7 @@ func approvalsChanged(from, to []address.Address) bool {
 	return false
 }
 
-func (t *transactionDiffer) Remove(key string, val *cbg.Deferred) error {
+func (t *transactionDiffContainer) Remove(key string, val *cbg.Deferred) error {
 	txID, err := abi.ParseIntKey(key)
 	if err != nil {
 		return err
@@ -130,4 +172,25 @@ func (t *transactionDiffer) Remove(key string, val *cbg.Deferred) error {
 		Tx:   tx,
 	})
 	return nil
+}
+
+func requiresLegacyDiffing(pre, cur State, pOpts, cOpts *adt.MapOpts) bool {
+	// hamt/v3 cannot read hamt/v2 nodes. Their Pointers struct has changed cbor marshalers.
+	if pre.Code() == builtin0.MultisigActorCodeID {
+		return true
+	}
+	if pre.Code() == builtin2.MultisigActorCodeID {
+		return true
+	}
+	if cur.Code() == builtin0.MultisigActorCodeID {
+		return true
+	}
+	if cur.Code() == builtin2.MultisigActorCodeID {
+		return true
+	}
+	// bitwidth or hashfunction differences mean legacy diffing.
+	if !pOpts.Equal(cOpts) {
+		return true
+	}
+	return false
 }
