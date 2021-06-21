a98973ac5e3f2057bdc6791bd74af3c6c511bc12
frrist <forrest@protocol.ai>
2021-06-21 16:23:41 -0700

    fix(api): add usage for --api flag
    - closes #552

--- commands/client.go
+++ commands/client.go
@@ -9,20 +9,24 @@ var clientAPIFlags struct {
 	apiToken string
 }
 
+var clientAPIFlag = &cli.StringFlag{
+	Name:        "api",
+	Usage:       "Address of visor api in multiaddr format.",
+	EnvVars:     []string{"VISOR_API"},
+	Value:       "/ip4/127.0.0.1/tcp/1234",
+	Destination: &clientAPIFlags.apiAddr,
+}
+
+var clientTokenFlag = &cli.StringFlag{
+	Name:        "api-token",
+	Usage:       "Authentication token for visor api.",
+	EnvVars:     []string{"VISOR_API_TOKEN"},
+	Value:       "",
+	Destination: &clientAPIFlags.apiToken,
+}
+
 // clientAPIFlagSet are used by commands that act as clients of a daemon's API
 var clientAPIFlagSet = []cli.Flag{
-	&cli.StringFlag{
-		Name:        "api",
-		Usage:       "Address of visor api in multiaddr format.",
-		EnvVars:     []string{"VISOR_API"},
-		Value:       "/ip4/127.0.0.1/tcp/1234",
-		Destination: &clientAPIFlags.apiAddr,
-	},
-	&cli.StringFlag{
-		Name:        "api-token",
-		Usage:       "Authentication token for visor api.",
-		EnvVars:     []string{"VISOR_API_TOKEN"},
-		Value:       "",
-		Destination: &clientAPIFlags.apiToken,
-	},
+	clientAPIFlag,
+	clientTokenFlag,
 }
