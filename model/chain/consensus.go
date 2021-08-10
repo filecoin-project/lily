da50470fde61bc1a99b448260235e3c79fdd0d73
Frrist <forrest@protocol.ai>
2021-08-10 16:14:57 -0700

    feat(task): implement consensus task (#615)
    * feat(task): implement consensus task
    - closes #614
    
    * polish(task): produce visor_processing_reports for null rounds

---
+++ model/chain/consensus.go
@@ -0,0 +1,48 @@
+package chain
+
+import (
+	"context"
+
+	"github.com/filecoin-project/sentinel-visor/metrics"
+	"github.com/filecoin-project/sentinel-visor/model"
+	"go.opencensus.io/tag"
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/api/trace"
+	"go.opentelemetry.io/otel/label"
+)
+
+type ChainConsensus struct {
+	Height          int64  `pg:",pk,notnull,use_zero"`
+	ParentStateRoot string `pg:",pk,notnull"`
+	ParentTipSet    string `pg:",pk,notnull"`
+	TipSet          string
+}
+
+func (c ChainConsensus) Persist(ctx context.Context, s model.StorageBatch, version model.Version) error {
+	ctx, span := global.Tracer("").Start(ctx, "ChainConsensus.Persist")
+	defer span.End()
+
+	ctx, _ = tag.New(ctx, tag.Upsert(metrics.Table, "chain_consensus"))
+	metrics.RecordCount(ctx, metrics.PersistModel, 1)
+	stop := metrics.Timer(ctx, metrics.PersistDuration)
+	defer stop()
+
+	return s.PersistModel(ctx, c)
+}
+
+type ChainConsensusList []*ChainConsensus
+
+func (c ChainConsensusList) Persist(ctx context.Context, s model.StorageBatch, version model.Version) error {
+	ctx, span := global.Tracer("").Start(ctx, "ChainConsensusList.Persist", trace.WithAttributes(label.Int("count", len(c))))
+	defer span.End()
+
+	ctx, _ = tag.New(ctx, tag.Upsert(metrics.Table, "chain_consensus"))
+	metrics.RecordCount(ctx, metrics.PersistModel, len(c))
+	stop := metrics.Timer(ctx, metrics.PersistDuration)
+	defer stop()
+
+	if len(c) == 0 {
+		return nil
+	}
+	return s.PersistModel(ctx, c)
+}
---
+++ tasks/consensus/task.go
@@ -0,0 +1,65 @@
+package consensus
+
+import (
+	"context"
+
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/sentinel-visor/model"
+	"github.com/filecoin-project/sentinel-visor/model/chain"
+	visormodel "github.com/filecoin-project/sentinel-visor/model/visor"
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/label"
+)
+
+type Task struct {
+}
+
+func NewTask() *Task {
+	return &Task{}
+}
+
+func (t *Task) ProcessTipSets(ctx context.Context, child, parent *types.TipSet) (model.Persistable, visormodel.ProcessingReportList, error) {
+	_, span := global.Tracer("").Start(ctx, "ProcessTipSets")
+	if span.IsRecording() {
+		span.SetAttributes(label.String("child", child.String()), label.Int64("height", int64(child.Height())))
+		span.SetAttributes(label.String("parent", parent.String()), label.Int64("height", int64(parent.Height())))
+	}
+	defer span.End()
+
+	var pl = make(chain.ChainConsensusList, child.Height()-parent.Height())
+	var rp = make(visormodel.ProcessingReportList, child.Height()-parent.Height())
+	idx := 0
+	for epoch := child.Height(); epoch > parent.Height(); epoch-- {
+		if child.Height() == epoch {
+			pl[idx] = &chain.ChainConsensus{
+				Height:          int64(epoch),
+				ParentStateRoot: child.ParentState().String(),
+				ParentTipSet:    child.Parents().String(),
+				TipSet:          child.Key().String(),
+			}
+			rp[idx] = &visormodel.ProcessingReport{
+				Height:    int64(epoch),
+				StateRoot: child.ParentState().String(),
+			}
+		} else {
+			// null round no tipset
+			pl[idx] = &chain.ChainConsensus{
+				Height:          int64(epoch),
+				ParentStateRoot: parent.ParentState().String(),
+				ParentTipSet:    parent.Parents().String(),
+				TipSet:          "",
+			}
+			rp[idx] = &visormodel.ProcessingReport{
+				Height:            int64(epoch),
+				StateRoot:         parent.ParentState().String(),
+				StatusInformation: "Null Round",
+			}
+		}
+		idx += 1
+	}
+	return pl, rp, nil
+}
+
+func (t *Task) Close() error {
+	return nil
+}
