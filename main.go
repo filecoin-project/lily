filecoin-project/sentinel-visor

feat: add observe command with peer agents task

main.go
@@ -138,6 +138,7 @@ func main() {
 			commands.LogCmd,
 			commands.MigrateCmd,
 			commands.NetCmd,
+			commands.ObserveCmd,
 			commands.RunCmd,
 			commands.StopCmd,
 			commands.SyncCmd,

filecoin-project/lily

main.go
@@ -146,6 +146,7 @@ func main() {
 			commands.LogCmd,
 			commands.MigrateCmd,
 			commands.NetCmd,
+			commands.SurveyCmd,
 			commands.StopCmd,
 			commands.SyncCmd,
 			commands.WaitApiCmd,