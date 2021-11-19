660efd721c9ce632c5f9ae4b4a07b3988f3c2091
Mike Greenberg <placer14@users.noreply.github.com>
2021-11-19 18:30:12 -0500

    chore: Remove unused test helper

--- chain/find_test.go
+++ chain/find_test.go
@@ -8,15 +8,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lily/model/visor"
 	"github.com/filecoin-project/lily/storage"
 	"github.com/filecoin-project/lily/testutil"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/go-pg/pg/v10"
-	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -412,23 +409,6 @@ type MockedFindLens struct {
 func (m *MockedFindLens) ChainGetTipSetByHeight(ctx context.Context, epoch abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error) {
 	args := m.Called(ctx, epoch, tsk)
 	return args.Get(0).(*types.TipSet), args.Error(1)
-}
-
-func fakeTipset(t testing.TB, height int) *types.TipSet {
-	bh := &types.BlockHeader{
-		Miner:                 address.TestAddress,
-		Height:                abi.ChainEpoch(height),
-		ParentStateRoot:       testutil.RandomCid(),
-		Parents:               []cid.Cid{testutil.RandomCid()},
-		Messages:              testutil.RandomCid(),
-		ParentMessageReceipts: testutil.RandomCid(),
-		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
-		Timestamp:             uint64(time.Now().Unix()),
-	}
-	ts, err := types.NewTipSet([]*types.BlockHeader{bh})
-	require.NoError(t, err)
-	return ts
 }
 
 type LoggingQueryHook struct{}
