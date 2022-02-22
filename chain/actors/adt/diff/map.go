428899b6288281797ebc5644a261b7e5e91bd46e
frrist <forrest@protocol.ai>
2022-02-22 13:14:16 -0800

    polish: add tracing to adt diff method
    - hamt diff
    - amt diff

--- chain/actors/adt/diff/map.go
+++ chain/actors/adt/diff/map.go
@@ -4,12 +4,17 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-hamt-ipld/v3"
-	adt2 "github.com/filecoin-project/lily/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"go.opentelemetry.io/otel"
+
+	adt2 "github.com/filecoin-project/lily/chain/actors/adt"
 )
 
 // Hamt returns a set of changes that transform `preMap` into `curMap`. opts are applied to both `preMap` and `curMap`.
 func Hamt(ctx context.Context, preMap, curMap adt2.Map, preStore, curStore adt.Store, hamtOpts ...hamt.Option) ([]*hamt.Change, error) {
+	ctx, span := otel.Tracer("").Start(ctx, "Hamt.Diff")
+	defer span.End()
+
 	preRoot, err := preMap.Root()
 	if err != nil {
 		return nil, err
