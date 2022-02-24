d6726e822166b792cab78853b424b6d5e7cd1f8f
frrist <forrest@protocol.ai>
2022-02-23 16:26:19 -0800

    refactor: break up internal message task per model extracte

--- tasks/messageexecutions/internal_parsed_message/task.go
+++ tasks/messageexecutions/internal_parsed_message/task.go
@@ -1,4 +1,4 @@
-package messageexecutions
+package internal_parsed_message
 
 import (
 	"context"
@@ -34,7 +34,7 @@ func (p *Task) ProcessTipSets(ctx context.Context, current *types.TipSet, execut
 			attribute.Int64("current_height", int64(current.Height())),
 			attribute.String("executed", executed.String()),
 			attribute.Int64("executed_height", int64(executed.Height())),
-			attribute.String("processor", "message_executions"),
+			attribute.String("processor", "internal_parsed_message"),
 		)
 	}
 	defer span.End()
@@ -51,7 +51,6 @@ func (p *Task) ProcessTipSets(ctx context.Context, current *types.TipSet, execut
 	}
 
 	var (
-		internalResult       = make(messagemodel.InternalMessageList, 0, len(mex))
 		internalParsedResult = make(messagemodel.InternalParsedMessageList, 0, len(mex))
 		errorsDetected       = make([]*messages.MessageError, 0) // we don't know the cap since mex is recursive in nature.
 	)
@@ -65,27 +64,6 @@ func (p *Task) ProcessTipSets(ctx context.Context, current *types.TipSet, execut
 
 		// we don't yet record implicit messages in the other message task, record them here.
 		if m.Implicit {
-			toName, toFamily, err := util.ActorNameAndFamilyFromCode(m.ToActorCode)
-			if err != nil {
-				errorsDetected = append(errorsDetected, &messages.MessageError{
-					Cid:   m.Cid,
-					Error: xerrors.Errorf("failed get message to actor name and family: %w", err).Error(),
-				})
-			}
-			internalResult = append(internalResult, &messagemodel.InternalMessage{
-				Height:        int64(m.Height),
-				Cid:           m.Cid.String(),
-				SourceMessage: "", // there is no source for implicit messages, they include cron tick and reward messages only
-				StateRoot:     m.StateRoot.String(),
-				From:          m.Message.From.String(),
-				To:            m.Message.To.String(),
-				ActorName:     toName,
-				ActorFamily:   toFamily,
-				Value:         m.Message.Value.String(),
-				Method:        uint64(m.Message.Method),
-				ExitCode:      int64(m.Ret.ExitCode),
-				GasUsed:       m.Ret.GasUsed,
-			})
 			method, params, err := util.MethodAndParamsForMessage(m.Message, m.ToActorCode)
 			if err != nil {
 				errorsDetected = append(errorsDetected, &messages.MessageError{
@@ -108,37 +86,5 @@ func (p *Task) ProcessTipSets(ctx context.Context, current *types.TipSet, execut
 	if len(errorsDetected) != 0 {
 		report.ErrorsDetected = errorsDetected
 	}
-	return model.PersistableList{
-		internalResult,
-		internalParsedResult,
-	}, report, nil
+	return internalParsedResult, report, nil
 }
-
-/*
-func walkExecutionTrace(et *types.ExecutionTrace, trace *[]*MessageTrace) {
-	for _, sub := range et.Subcalls {
-		*trace = append(*trace, &MessageTrace{
-			Message:   sub.Msg,
-			Receipt:   sub.MsgRct,
-			Error:     sub.Error,
-			Duration:  sub.Duration,
-			GasCharge: sub.GasCharges,
-		})
-		walkExecutionTrace(&sub, trace) //nolint:scopelint,gosec
-	}
-}
-
-type MessageTrace struct {
-	Message   *types.Message
-	Receipt   *types.MessageReceipt
-	Error     string
-	Duration  time.Duration
-	GasCharge []*types.GasTrace
-}
-
-func getChildMessagesOf(m *lens.MessageExecution) []*MessageTrace {
-	var out []*MessageTrace
-	walkExecutionTrace(&m.Ret.ExecutionTrace, &out)
-	return out
-}
-*/
