4928cad85e62852eb1b6682311ecc577be0d1012
frrist <forrest@protocol.ai>
2022-04-27 12:16:42 -0700

    docs: add find description

--- commands/job/find.go
+++ commands/job/find.go
@@ -12,7 +12,22 @@ import (
 
 var GapFindCmd = &cli.Command{
 	Name:  "find",
-	Usage: "find gaps in the database",
+	Usage: "find gaps in the database for a given range and a set of tasks.",
+	Description: `
+The find job searches for gaps in a database storage system by executing the SQL gap_find() function over the visor_processing_reports table.
+find will query the database for gaps based on the list of tasks (--tasks) provided over the specified range (--to --from).
+An epoch is considered to have gaps iff:
+- a task specified by the --task flag is not present at each epoch within the specified range.
+- a task specified by the --task flag does not have status 'OK' at each epoch within the specified range.
+The results of the find job are written to the visor_gap_reports table.
+
+As an example, the below command:
+ $ lily job run --tasks=block_headers,messages find --from=10 --to=20
+searches for gaps in block_headers and messages tasks from epoch 10 to 20 (inclusive). 
+
+Note: the find job should NOT be executed against heights that were imported from historical data dumps: https://lilium.sh/data/dumps/ 
+since visor_processing_report entries will not be present for imported data.
+`,
 	Flags: []cli.Flag{
 		RangeFromFlag,
 		RangeToFlag,
