c82a4e335245ed7e1b65646456efcb6e9885a7db
Steph Samson <hello@stephsamson.com>
2022-07-27 14:42:10 +0200

    feat: add index on events covering height and miner_id

---
+++ schemas/v1/8_miner_sector_events_idx.go
@@ -0,0 +1,14 @@
+package v1
+
+func init() {
+	patches.Register(
+		8,
+		`
+------------------------------------------------------------------
+-- Name: miner_sector_events_event_idx
+-- Model: miner.MinerSectorEvent
+-- ----------------------------------------------------------------
+CREATE INDEX miner_sector_events_event_idx ON {{ .SchemaName | default "public"}}.miner_sector_events USING btree(event) INCLUDE(height, miner_id);
+`,
+	)
+}
