4e85dee8386514bf46921e6f2f0288e325268f94
Steph Samson <hello@stephsamson.com>
2022-07-27 17:21:54 +0200

    chore: rm concurrently entirely and do transaction on chunk

--- schemas/v1/8_miner_sector_events_idx.go
+++ schemas/v1/8_miner_sector_events_idx.go
@@ -8,7 +8,7 @@ func init() {
 -- Name: miner_sector_events_event_idx
 -- Model: miner.MinerSectorEvent
 -- ----------------------------------------------------------------
-CREATE INDEX CONCURRENTLY miner_sector_events_event_idx ON {{ .SchemaName | default "public"}}.miner_sector_events
+CREATE INDEX miner_sector_events_event_idx ON {{ .SchemaName | default "public"}}.miner_sector_events
 	USING btree(event) INCLUDE(height, miner_id) WITH(timescaledb.transaction_per_chunk);
 `,
 	)
