05c67f5ea12169ab09d6e29ffae8b8d6287c1062
Steph Samson <hello@stephsamson.com>
2022-07-27 15:54:21 +0200

    chore: create index concurrently per @iand

--- schemas/v1/8_miner_sector_events_idx.go
+++ schemas/v1/8_miner_sector_events_idx.go
@@ -8,7 +8,7 @@ func init() {
 -- Name: miner_sector_events_event_idx
 -- Model: miner.MinerSectorEvent
 -- ----------------------------------------------------------------
-CREATE INDEX miner_sector_events_event_idx ON {{ .SchemaName | default "public"}}.miner_sector_events USING btree(event) INCLUDE(height, miner_id);
+CREATE INDEX CONCURRENTLY miner_sector_events_event_idx ON {{ .SchemaName | default "public"}}.miner_sector_events USING btree(event) INCLUDE(height, miner_id);
 `,
 	)
 }
