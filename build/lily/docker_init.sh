da510c3c6ad04f56c3d77ec5d38d64e57803f0e5
Mike Greenberg <305332+placer14@users.noreply.github.com>
2022-08-29 17:55:56 -0400

    feat(docker): Update docker-compose to use lily worker pattern (#1044)
    * feat(docker): Update docker-compose to use lily worker pattern
    
    * fix: Snapshot envvar; prom host; env quotes
    
    * chore: Address comments
    
    Co-authored-by: Mike Greenberg <placer14@users.noreply.github.com>

--- build/lily/docker_init.sh
+++ build/lily/docker_init.sh
@@ -4,14 +4,17 @@ set -exo pipefail
 
 mkdir -p ${LILY_REPO}/keystore
 
-if [[ ! -z "${_LILY_DOCKER_INIT_IMPORT_MAINNET_SNAPSHOT}" ]]; then
-  # import snapshot when _LILY_DOCKER_INIT_IMPORT_MAINNET_SNAPSHOT is set
+
+if [[ ! -z "${LILY_DOCKER_INIT_IMPORT_MAINNET_SNAPSHOT}" ]]; then
+  # set default snapshot path if not already defined
+  snapshot="${LILY_DOCKER_INIT_IMPORT_SNAPSHOT_PATH:-https://snapshots.mainnet.filops.net/minimal/latest}"
+
+  # import snapshot when LILY_DOCKER_INIT_IMPORT_MAINNET_SNAPSHOT is set
   if [[ -f "${LILY_REPO}/datastore/_imported" ]]; then
     echo "Skipping import, found ${LILY_REPO}/datastore/_imported file."
   else
-    echo "Importing snapshot from https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_finality_stateroots_latest.car..."
-    lily init --import-snapshot="https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_finality_stateroots_latest.car"
-
+    echo "Importing snapshot from ${snapshot}"
+    lily init --import-snapshot=${snapshot}
     status=$?
     if [ $status -eq 0 ]; then
       touch "/var/lib/lily/datastore/_imported"
@@ -22,6 +25,6 @@ else
   lily init
 fi
 
-chmod 0600 ${LILY_REPO}/keystore/*
+chmod -R 0600 ${LILY_REPO}/keystore
 
 lily $@
