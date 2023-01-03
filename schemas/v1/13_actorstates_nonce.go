d9fa6d6570d4232af0d5a06e55483ffc28dd585b
Steph Samson <hello@stephsamson.com>
2023-01-03 12:46:15 +0900

    chore: add uniq_nullable on nonce

--- schemas/v1/13_actorstates_nonce.go
+++ schemas/v1/13_actorstates_nonce.go
@@ -5,7 +5,8 @@ func init() {
 		13,
 		`
 ALTER TABLE {{ .SchemaName | default "public"}}.actor_states
-ADD COLUMN nonce BIGINT;
+  ADD COLUMN nonce BIGINT
+  ADD CONSTRAINT uniq_nullable_nonce UNIQUE (head,nonce);
 
 COMMENT ON COLUMN {{ .SchemaName | default "public"}}.actor_states.nonce IS 'The nonce of the actor expected to appear on the chain after the actor has been modified or created at each epoch. More precisely, this nonce tracks the number of messages sent by an actor.';
 `,
