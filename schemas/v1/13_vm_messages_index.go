0b2982a79128fe13b23cfd99a0904a688d445e48
birdy <birdychang@gmail.com>
2023-03-01 11:34:19 -0800

    Add comment

--- schemas/v1/13_vm_messages_index.go
+++ schemas/v1/13_vm_messages_index.go
@@ -6,7 +6,7 @@ func init() {
 		`
 ALTER TABLE {{ .SchemaName | default "public"}}.vm_messages
     ADD COLUMN IF NOT EXISTS "index" BIGINT NOT NULL DEFAULT -1;
-
+-- dropping the default value since it's only needed for existing data to not violate no null constraint
 ALTER TABLE {{ .SchemaName | default "public"}}.vm_messages ALTER COLUMN "index" DROP DEFAULT;
 
 COMMENT ON COLUMN {{ .SchemaName | default "public"}}.vm_messages.index IS 'Order in which the message was applied.';
