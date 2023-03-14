86e4cc0814e503b3ba518093e11aff29dcd048c5
Steph Samson <hello@stephsamson.com>
2023-03-14 14:48:35 +0800

    fix typo

--- schemas/v1/19_actor_mappings.go
+++ schemas/v1/19_actor_mappings.go
@@ -1,20 +1,18 @@
 package v1
 
-// Schema version 16 adds actor_codes and actor_methods mappings
-
 func init() {
 	patches.Register(
 		19,
 		`
-	CREATE TABLE IF NOT EXISTS {{ .SchemaName | default "public"}}.actor_codes (
-		cid		text  NOT NULL,
-		code	text  NOT NULL,
-		
-		PRIMARY KEY (cid, code)
-	);
-	COMMENT ON TABLE {{ .SchemaName | default "public"}}.actor_codes IS 'A mapping of a builtin actor's' CID to a human friendly name.';
-	COMMENT ON COLUMN {{ .SchemaName | default "public"}}.actor_codes.cid IS 'CID of the actor from builtin actors.';
-	COMMENT ON COLUMN {{ .SchemaName | default "public"}}.actor_codes.code IS 'Human-readable identifier for the actor.';
+CREATE TABLE IF NOT EXISTS {{ .SchemaName | default "public"}}.actor_codes (
+	cid		text  NOT NULL,
+	code	text  NOT NULL,
+	
+	PRIMARY KEY (cid, code)
+);
+COMMENT ON TABLE {{ .SchemaName | default "public"}}.actor_codes IS 'A mapping of a builtin actors CID to a human friendly name.';
+COMMENT ON COLUMN {{ .SchemaName | default "public"}}.actor_codes.cid IS 'CID of the actor from builtin actors.';
+COMMENT ON COLUMN {{ .SchemaName | default "public"}}.actor_codes.code IS 'Human-readable identifier for the actor.';
 `,
 	)
 }
