14dc9839da32cb31f2a4be32829db1f1a589c694
Steph Samson <ayo@kasteph.com>
2023-04-18 18:42:11 +0200

    fix:

--- chain/indexer/tasktype/tasks_test.go
+++ chain/indexer/tasktype/tasks_test.go
@@ -101,7 +101,7 @@ func TestMakeAllTaskAliasNames(t *testing.T) {
 }
 
 func TestMakeAllTaskNames(t *testing.T) {
-	const TotalTableTasks = 44
+	const TotalTableTasks = 43
 	actual, err := tasktype.MakeTaskNames(tasktype.AllTableTasks)
 	require.NoError(t, err)
 	// if this test fails it means a new task name was added, update the above test
