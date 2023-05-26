3116453befeccc8e7d5052764f74f3b96933df53
Terry <terry@TerrydeMacBook-Pro.local>
2023-05-26 12:17:17 +0800

    Add new schema for fevm transaction

---
+++ schemas/v1/24_fevm_transaction.go
@@ -0,0 +1,31 @@
+package v1
+
+func init() {
+	patches.Register(
+		24,
+		`
+	CREATE TABLE IF NOT EXISTS {{ .SchemaName | default "public"}}.fevm_transaction  (
+	    height               BIGINT NOT NULL,
+		hash                 TEXT,
+		transaction_index    BIGINT,
+		block_hash           TEXT,
+		block_number         BIGINT,
+		chain_id             BIGINT,
+		nonce                BIGINT,
+		type                 BIGINT,
+		"from"               TEXT,
+		"to"                 TEXT,
+		input                TEXT,
+		value                TEXT,
+		gas                  BIGINT,
+		max_fee_per_gas      TEXT,
+		max_priority_fee_per_gas TEXT,
+		v                    TEXT,
+		r                    TEXT,
+		s                    TEXT,
+		access_list          TEXT[],
+		PRIMARY KEY(height, hash)
+	);
+`,
+	)
+}
