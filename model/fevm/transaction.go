0755a91917037baeba08dd5ff026fa76cd610097
terry.hung <terry.hung@protocol.ai>
2023-08-23 17:07:33 +0800

    Refine the model comment

--- model/fevm/transaction.go
+++ model/fevm/transaction.go
@@ -53,11 +53,11 @@ type FEVMTransaction struct {
 	FromFilecoinAddress string `pg:",notnull"`
 	// Filecoin Address of the receiver.
 	ToFilecoinAddress string `pg:",notnull"`
-	// Human-readable identifier of receiver (To).
-	FromActorName string `pg:",notnull"`
-	// On-chain message triggering the message.
-	ToActorName string `pg:",notnull"`
 	// Human-readable identifier of sender (From).
+	FromActorName string `pg:",notnull"`
+	// Human-readable identifier of receiver (To).
+	ToActorName string `pg:",notnull"`
+	// On-chain message triggering the message.
 	MessageCid string `pg:",pk,notnull"`
 }
 
