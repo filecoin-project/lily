162d67f95a3f91db480178c1f89a20a8480c5036
terryhung <terryhung1228@gmail.com>
2025-04-15 11:44:58 +0800

    feat: add more log

--- chain/indexer/exporter.go
+++ chain/indexer/exporter.go
@@ -42,6 +42,7 @@ type ModelResult struct {
 // - if data with height N and SR2 is being persisted and a request to persist data with height N and SR1 is made, block
 func (me *ModelExporter) ExportResult(ctx context.Context, strg model.Storage, height int64, results []*ModelResult) error {
 	if len(results) == 0 {
+		log.Debugw("no results to export", "height", height, "reporter", me.name)
 		return nil
 	}
 	// lock exporting based on height only allowing a single height to be persisted simultaneously
