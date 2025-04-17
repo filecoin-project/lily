7eec6927b5c7f6d40dff54a7cfc12c0978014193
terryhung <terryhung1228@gmail.com>
2025-04-17 14:09:11 +0800

    chore: update the comment for new field

--- schemas/v1/45_add_fields_in_miner_dump.go
+++ schemas/v1/45_add_fields_in_miner_dump.go
@@ -5,6 +5,7 @@ func init() {
 		45,
 		`
 		ALTER TABLE {{ .SchemaName | default "public"}}.miner_actor_dumps ADD COLUMN IF NOT EXISTS termination_fee_v2 numeric DEFAULT 0;
+		COMMENT ON COLUMN {{ .SchemaName | default "public"}}.miner_actor_dumps IS 'A penalty imposed when a sector is prematurely terminated in attoFIL (after nv25).';
 `,
 	)
 }
